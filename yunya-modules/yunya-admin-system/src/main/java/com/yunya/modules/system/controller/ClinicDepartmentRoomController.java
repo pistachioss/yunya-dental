f23d297bce03c8caf584ab09fb6b62801368c933
zhou_wen <sa52305@outlook.com>
2020-07-20 14:59:35 +0800

    新增科室、门诊科室管理

---
+++ yunya-modules/yunya-admin-system/src/main/java/com/yunya/modules/system/controller/ClinicDepartmentRoomController.java
@@ -0,0 +1,18 @@
+package com.yunya.modules.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 简介:
+ *
+ * @author: chow
+ * @date: 2020/7/20 14:56
+ * @description:
+ * @since: 1.0.0
+ */
+@RestController
+@RequestMapping("dept")
+public class ClinicDepartmentRoomController {
+    /***/
+}
