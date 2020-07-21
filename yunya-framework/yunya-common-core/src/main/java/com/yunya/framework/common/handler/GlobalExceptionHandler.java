af8cb8e922f5a695714fe09754551f7340b9012f
xiangyang <1480447105@qq.com>
2020-07-21 20:03:46 +0800

    feat(统一异常管理): 统一异常管理
    统一异常管理

--- yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/handler/GlobalExceptionHandler.java
+++ yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/handler/GlobalExceptionHandler.java
@@ -1,13 +1,12 @@
 package com.yunya.framework.common.handler;
 
-import com.alibaba.fastjson.JSON;
 import com.yunya.framework.common.constant.CommonConstants;
-import com.yunya.framework.common.exception.BaseException;
 import com.yunya.framework.common.exception.ClientServiceException;
 import com.yunya.framework.common.exception.auth.ClientTokenException;
 import com.yunya.framework.common.exception.auth.UserAuthException;
 import com.yunya.framework.common.exception.auth.UserTokenException;
-import com.yunya.framework.common.model.BaseResponse;
+import com.yunya.framework.common.model.ResponseResult;
+import com.yunya.framework.common.utils.ResponseUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +14,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -34,47 +31,46 @@ public class GlobalExceptionHandler {
   private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
   @ExceptionHandler(ClientTokenException.class)
-  public BaseResponse clientTokenExceptionHandler(
+  public ResponseResult clientTokenExceptionHandler(
       HttpServletResponse response, ClientTokenException ex) {
     response.setStatus(403);
     log.error(ex.getMessage(), ex);
-    return new BaseResponse(ex.getStatus(), ex.getMessage());
+    return ResponseUtil.result(ex.getStatus(),ex.getMessage(),null,true);
   }
 
   @ExceptionHandler(UserTokenException.class)
-  public BaseResponse userTokenExceptionHandler(
+  public ResponseResult userTokenExceptionHandler(
       HttpServletResponse response, UserTokenException ex) {
     response.setStatus(200);
     log.error(ex.getMessage(), ex);
-    return new BaseResponse(ex.getStatus(), ex.getMessage());
+    return ResponseUtil.result(ex.getStatus(),ex.getMessage(),null,true);
   }
 
   @ExceptionHandler(UserAuthException.class)
-  public BaseResponse userInvalidExceptionHandler(
+  public ResponseResult userInvalidExceptionHandler(
       HttpServletResponse response, UserAuthException ex) {
     response.setStatus(200);
     log.error(ex.getMessage(), ex);
-    return new BaseResponse(ex.getStatus(), ex.getMessage());
+    return ResponseUtil.result(ex.getStatus(),ex.getMessage(),null,true);
   }
 
   @ExceptionHandler(ClientServiceException.class)
-  public BaseResponse baseExceptionHandler(HttpServletResponse response, BaseException ex) {
+  public ResponseResult baseExceptionHandler(HttpServletResponse response, ClientServiceException ex) {
     log.error(ex.getMessage(), ex);
-    response.setStatus(500);
-    return new BaseResponse(CommonConstants.EX_OTHER_CODE, ex.getMessage());
+    return ResponseUtil.result(ex.getStatus(),ex.getMessage(),null,true);
   }
 
   @ExceptionHandler(Exception.class)
-  public BaseResponse otherExceptionHandler(HttpServletResponse response, Exception ex) {
+  public ResponseResult otherExceptionHandler(HttpServletResponse response, Exception ex) {
     response.setStatus(500);
     log.error(ex.getMessage(), ex);
-    return new BaseResponse(CommonConstants.EX_OTHER_CODE, ex.getMessage());
+    return ResponseUtil.result(CommonConstants.EX_OTHER_CODE,ex.getMessage(),null,true);
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public BaseResponse methodArgumentNotValidHandler(MethodArgumentNotValidException e, HttpServletRequest request) {
+  public ResponseResult methodArgumentNotValidHandler(MethodArgumentNotValidException e, HttpServletRequest request) {
     String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
     logger.error("error in \nurl :{} \nmsg:{}",request.getRequestURI(),message);
-    return new BaseResponse(CommonConstants.EX_OTHER_CODE, message);
+    return ResponseUtil.result(CommonConstants.EX_OTHER_CODE, message,null,true);
   }
 }
