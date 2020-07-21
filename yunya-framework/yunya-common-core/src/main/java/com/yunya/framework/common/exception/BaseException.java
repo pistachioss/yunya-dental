a04376544aefa3c82c5c02cd4ee5465a933003bd
zhou_wen <sa52305@outlook.com>
2020-07-21 20:59:35 +0800

    更新异常状态

--- yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/exception/BaseException.java
+++ yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/exception/BaseException.java
@@ -8,7 +8,7 @@ package com.yunya.framework.common.exception;
  */
 public class BaseException extends RuntimeException {
 
-  private int status = 200;
+  private int status = 0;
 
   public int getStatus() {
     return status;
