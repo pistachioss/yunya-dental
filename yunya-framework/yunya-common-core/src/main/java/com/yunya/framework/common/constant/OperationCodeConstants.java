e79b90b2676f227a13d4b857e16a5c50ccc3e1d0
me <1123038904@qq.com>
2020-07-23 09:41:38 +0800

    字段添加

--- yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/constant/OperationCodeConstants.java
+++ yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/constant/OperationCodeConstants.java
@@ -28,4 +28,6 @@ public class OperationCodeConstants {
   public static final Integer RETURN_VALUE_ISNULL = 30008;
   /** 数据不存在 */
   public static final Integer DATA_NOT_EXIST = 30009;
+  /** 时间格式转换异常 */
+  public static final Integer DATA_TRANSFORMATION_EXIST = 30010;
 }
