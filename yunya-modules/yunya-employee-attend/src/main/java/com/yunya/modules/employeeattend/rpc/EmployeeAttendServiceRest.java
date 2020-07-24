0ca4594797695a313698e147b3ab8f1e784d1eea
me <1123038904@qq.com>
2020-07-24 16:34:37 +0800

    排班111

--- yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/rpc/EmployeeAttendServiceRest.java
+++ yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/rpc/EmployeeAttendServiceRest.java
@@ -26,7 +26,7 @@ public class EmployeeAttendServiceRest {
 
   @Autowired private EmployeeScheduleSerivce employeeScheduleSerivce;
   /**
-   * 查看员工排班列表
+   * 查看员工排班列表暴露
    * @param employeeScheduleQueryForm
    * @return
    */
