833934420b7fd0ab1f6f7c4e6b4667776c5ab23e
zhou_wen <sa52305@outlook.com>
2020-07-31 11:23:13 +0800

    更新卡券折扣

--- yunya-modules/yunya-clinic-discount/src/main/java/com/yunya/modules/discount/mapper/MarketProductTypeMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yunya.modules.discount.mapper;
-
-import com.yunya.models.discount.MarketProductType;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface MarketProductTypeMapper extends Mapper<MarketProductType> {
-}
