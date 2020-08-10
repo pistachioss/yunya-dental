d7303499306facccda6402f298657e10b39be246
zhou_wen <sa52305@outlook.com>
2020-08-10 17:52:55 +0800

    更新价目表相关

--- yunya-remote/src/main/java/com/yunya/feign/tariff/RemoteTariffServiceFeign.java
+++ yunya-remote/src/main/java/com/yunya/feign/tariff/RemoteTariffServiceFeign.java
@@ -1,8 +1,18 @@
 package com.yunya.feign.tariff;
 
-import com.yunya.feign.system.factory.RemoteSystemServiceFallBackFactory;
+import com.yunya.feign.tariff.factory.RemoteTariffServiceFeignFallBackFactory;
 import com.yunya.framework.common.constant.YunyaServiceNameConstants;
+import com.yunya.models.tariff.BaseOralTariff;
+import com.yunya.models.tariff.BaseOralTariffCategory;
+import com.yunya.models.tariff.BaseTariff;
+import com.yunya.models.tariff.BaseTariffCategory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * 简介: 云牙价目表服务接口调用
@@ -14,7 +24,77 @@ import org.springframework.cloud.openfeign.FeignClient;
  */
 @FeignClient(
     name = YunyaServiceNameConstants.YUNYA_TARIFF_SERVICE,
-    fallbackFactory = RemoteSystemServiceFallBackFactory.class)
+    fallbackFactory = RemoteTariffServiceFeignFallBackFactory.class)
 public interface RemoteTariffServiceFeign {
-  /***/
+  /**
+   * 根据商品分类ID查询商品分类信息
+   *
+   * @param id 商品分类ID
+   * @return
+   */
+  @RequestMapping(value = "/oral/category/{id}", method = RequestMethod.GET)
+  BaseOralTariffCategory findBaseOralTariffCategoryById(@PathVariable(value = "id") Integer id);
+
+  /**
+   * 根据条件查询门诊商品分类列表
+   *
+   * @param queryForm 查询条件
+   * @return
+   */
+  @RequestMapping(value = "/oral/category/list", method = RequestMethod.POST)
+  List<BaseOralTariffCategory> findBaseOralTariffCategoryList(
+      @RequestBody BaseOralTariffCategory queryForm);
+
+  /**
+   * 根据商品项目ID查询商品项目信息
+   *
+   * @param id 商品项目ID
+   * @return
+   */
+  @RequestMapping(value = "/oral/one/{id}", method = RequestMethod.GET)
+  BaseOralTariff findBaseOralTariffById(@PathVariable(value = "id") Integer id);
+  /**
+   * 根据条件查询商品项目列表
+   *
+   * @param entity 查询条件
+   * @return
+   */
+  @RequestMapping(value = "/oral/list", method = RequestMethod.POST)
+  List<BaseOralTariff> findBaseOralTariffList(@RequestBody BaseOralTariff entity);
+
+  /**
+   * 根据价目表分类ID查询价目表分类信息
+   *
+   * @param id 价目表分类ID
+   * @return
+   */
+  @RequestMapping(value = "/base/one/{id}", method = RequestMethod.GET)
+  BaseTariffCategory findBaseTariffCategoryById(@PathVariable(value = "id") Integer id);
+
+  /**
+   * 根据条件查询价目表分类列表
+   *
+   * @param entity 查询条件
+   * @return
+   */
+  @RequestMapping(value = "/base/list", method = RequestMethod.POST)
+  List<BaseTariffCategory> findBaseTariffCategoryList(@RequestBody BaseTariffCategory entity);
+
+  /**
+   * 根据价目表项目ID查询基础价目表信息
+   *
+   * @param id 基础价目表ID
+   * @return
+   */
+  @RequestMapping(value = "/tariff/{id}", method = RequestMethod.GET)
+  BaseTariff findBaseTariffById(@PathVariable(value = "id") Integer id);
+
+  /**
+   * 根据条件查询价目表列表
+   *
+   * @param entity 查询条件
+   * @return
+   */
+  @RequestMapping(value = "/tariff/list", method = RequestMethod.POST)
+  List<BaseTariff> findBaseTariffList(@RequestBody BaseTariff entity);
 }
