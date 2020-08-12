116e3a6f4a6f49838d0deac462cb8206dea33420
zhou_wen <sa52305@outlook.com>
2020-08-12 14:59:23 +0800

    新增接诊相关

---
+++ yunya-modules/yunya-treatment/src/main/java/com/yunya/modules/treatment/mapper/TreatmentRecordMapper.java
@@ -0,0 +1,7 @@
+package com.yunya.modules.treatment.mapper;
+
+import com.yunya.models.treatment.TreatmentRecord;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface TreatmentRecordMapper extends Mapper<TreatmentRecord> {
+}
