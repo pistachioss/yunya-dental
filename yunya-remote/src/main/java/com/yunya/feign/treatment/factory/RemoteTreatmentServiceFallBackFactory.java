6d044cff8743079eee8f14f27adad3ba90d5aa63
wy <wangye138386>
2020-08-15 16:07:39 +0800

    //患者服务修改

--- yunya-remote/src/main/java/com/yunya/feign/treatment/factory/RemoteTreatmentServiceFallBackFactory.java
+++ yunya-remote/src/main/java/com/yunya/feign/treatment/factory/RemoteTreatmentServiceFallBackFactory.java
@@ -1,8 +1,12 @@
 package com.yunya.feign.treatment.factory;
 
 import com.yunya.feign.treatment.RemoteTreatmentServiceFeign;
+import com.yunya.models.treatment.Registered;
+import com.yunya.models.treatment.TreatmentRecord;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * 简介: 云牙诊疗服务降级处理
@@ -15,5 +19,29 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class RemoteTreatmentServiceFallBackFactory implements RemoteTreatmentServiceFeign {
-  /***/
+    @Override
+    public Registered findRegisteredById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Registered> findRegisteredList(Registered entity) {
+        return null;
+    }
+
+    @Override
+    public TreatmentRecord findTreatmentRecordById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<TreatmentRecord> findTreatmentRecordList(TreatmentRecord entity) {
+        return null;
+    }
+
+    @Override
+    public void updateTreatmentRecord(Integer id) {
+
+    }
+    /***/
 }
