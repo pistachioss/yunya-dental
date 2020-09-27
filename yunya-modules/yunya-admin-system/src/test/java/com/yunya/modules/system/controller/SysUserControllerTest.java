182207976778a7389a299a1fd569093ab8e19fe1
zhou_wen <sa52305@outlook.com>
2020-09-27 13:16:26 +0800

    更新根据条件查询员工信息

--- yunya-modules/yunya-admin-system/src/test/java/com/yunya/modules/system/controller/SysUserControllerTest.java
+++ yunya-modules/yunya-admin-system/src/test/java/com/yunya/modules/system/controller/SysUserControllerTest.java
@@ -28,18 +28,18 @@ public class SysUserControllerTest {
   @Test
   public void testFindUserListByExample() {
     SysUserInfoDetailQueryFrom form = new SysUserInfoDetailQueryFrom();
-    form.setKeyWord("158");
-    //    List<Integer> orgIds = new ArrayList<>();
+    //form.setKeyWord("158");
+        List<Integer> orgIds = new ArrayList<>();
     //    orgIds.add(21);
-    //    orgIds.add(35);
+        orgIds.add(35);
+        form.setOrgIds(orgIds);
     //    List<Integer> postIds = new ArrayList<>();
     //    postIds.add(33);
     //    postIds.add(34);
-    //    //form.put("userId",1);
-    //    List<Integer> groupIds = new ArrayList<>();
-    //    groupIds.add(3);
+        List<Integer> groupIds = new ArrayList<>();
+        groupIds.add(3);
     //    groupIds.add(4);
-    //    form.setPostGroupIds(groupIds);
+        form.setPostGroupIds(groupIds);
     ResponseResult list = sysUserController.findList(form);
     System.out.println(list);
   }
