8176c3e7643377b124cfd80daf4489df6e09c62a
xiangyang <1480447105@qq.com>
2020-10-15 11:25:25 +0800

    feat(middle): fix fix bug

--- yunya-remote/src/main/java/com/yunya/feign/discount/domain/bo/PatientUseBenefitBo.java
+++ yunya-remote/src/main/java/com/yunya/feign/discount/domain/bo/PatientUseBenefitBo.java
@@ -20,72 +20,78 @@ import static java.util.stream.Collectors.*;
 @Getter
 @Setter
 public class PatientUseBenefitBo implements Serializable {
-    private Integer couponId;
-    private Integer couponType;
-    private BigDecimal discountRate;
-    private List<Integer> usableClinic;
-    private BigDecimal face;
-    private BigDecimal balance;
-    private Integer cardId;
-    private String cardNumber;
-    private String couponName;
-    private LocalDate useDeadline;
-    private Integer mixable;
-    private Integer useWay;
-    private Integer limitCount;
-    /**
-     * 已经使用次数
-     */
-    private Integer usedTimes;
-    private List<ItemBenefitUseDetailBo> benefitUseDetail;
+	private Integer couponId;
+	private Integer couponType;
+	private BigDecimal discountRate;
+	private List<Integer> usableClinic;
+	private BigDecimal face;
+	private BigDecimal balance;
+	private Integer cardId;
+	private String cardNumber;
+	private String couponName;
+	private LocalDate useDeadline;
+	private Integer mixable;
+	private Integer useWay;
+	private Integer limitCount;
+	/**
+	 * 已经使用次数
+	 */
+	private Integer usedTimes;
+	private List<ItemBenefitUseDetailBo> benefitUseDetail;
 
-    public PatientUseBenefitBo() {
-        this.usedTimes = 0;
-        this.benefitUseDetail = Lists.newArrayList();
-    }
+	public PatientUseBenefitBo() {
+		this.usedTimes = 0;
+		this.benefitUseDetail = Lists.newArrayList();
+	}
 
-    public void setExchangeCouponItemDetail(List<CouponItemUseBo> list) {
-        if (CollectionUtils.isNotEmpty(list)) {
-            List<ItemBenefitUseDetailBo> itemList = list.stream().map(exchangeItem -> {
-                return new ItemBenefitUseDetailBo(exchangeItem.getItemId(), exchangeItem.getType(),
-                        exchangeItem.getOriginCount() - exchangeItem.getUseCount(), BigDecimal.valueOf(0));
-            }).collect(toList());
-            this.setBenefitUseDetail(itemList);
-        }
-    }
+	public void setExchangeCouponItemDetail(List<CouponItemUseBo> list) {
+		int one = 1;
+		if (CollectionUtils.isNotEmpty(list)) {
+			List<ItemBenefitUseDetailBo> itemList = list.stream().filter(obj -> obj.getUsable() == one)
+					.map(exchangeItem -> {
+						return new ItemBenefitUseDetailBo(exchangeItem.getItemId(), exchangeItem.getType(),
+								exchangeItem.getOriginCount() - exchangeItem.getUseCount(), BigDecimal.valueOf(0));
 
-    public void setPackageCouponItemDetail(List<CouponItemUseBo> list) {
-        if (CollectionUtils.isNotEmpty(list)) {
-            List<ItemBenefitUseDetailBo> itemList = list.stream().map(packageItem -> {
-                return new ItemBenefitUseDetailBo(packageItem.getItemId(), packageItem.getType(),
-                        packageItem.getOriginCount() - packageItem.getUseCount(), packageItem.getPackageUnitPrice());
-            }).collect(toList());
-            this.setBenefitUseDetail(itemList);
-        }
-    }
+					}).collect(toList());
+			this.setBenefitUseDetail(itemList);
+		}
+	}
 
-    public void setDiscountVoucherCouponItemDetail(Set<Integer> set, Integer type, Integer rangType) {
-        if (rangType == 0) {
-            int itemId = 0;
-            List<ItemBenefitUseDetailBo> choiceAllList = Lists.newArrayList(new ItemBenefitUseDetailBo(itemId, type, null, null));
-            addUseDetail(choiceAllList);
-        } else {
-            if (CollectionUtils.isNotEmpty(set)) {
-                List<ItemBenefitUseDetailBo> itemList = set.stream().map(itemId -> {
-                    return new ItemBenefitUseDetailBo(itemId, type, null, null);
-                }).collect(toList());
-                addUseDetail(itemList);
-            }
-        }
-    }
+	public void setPackageCouponItemDetail(List<CouponItemUseBo> list) {
+		int one = 1;
+		if (CollectionUtils.isNotEmpty(list)) {
+			List<ItemBenefitUseDetailBo> itemList = list.stream().filter(obj -> obj.getUsable() == one)
+					.map(packageItem -> {
+						return new ItemBenefitUseDetailBo(packageItem.getItemId(), packageItem.getType(),
+								packageItem.getOriginCount() - packageItem.getUseCount(), packageItem.getPackageUnitPrice());
 
-    private void addUseDetail(List<ItemBenefitUseDetailBo> itemList) {
-        List<ItemBenefitUseDetailBo> useDetail = this.getBenefitUseDetail();
-        if (CollectionUtils.isNotEmpty(useDetail)){
-            useDetail.addAll(itemList);
-        } else {
-            this.setBenefitUseDetail(itemList);
-        }
-    }
+					}).collect(toList());
+			this.setBenefitUseDetail(itemList);
+		}
+	}
+
+	public void setDiscountVoucherCouponItemDetail(Set<Integer> set, Integer type, Integer rangType) {
+		if (rangType == 0) {
+			int itemId = 0;
+			List<ItemBenefitUseDetailBo> choiceAllList = Lists.newArrayList(new ItemBenefitUseDetailBo(itemId, type, null, null));
+			addUseDetail(choiceAllList);
+		} else {
+			if (CollectionUtils.isNotEmpty(set)) {
+				List<ItemBenefitUseDetailBo> itemList = set.stream().map(itemId -> {
+					return new ItemBenefitUseDetailBo(itemId, type, null, null);
+				}).collect(toList());
+				addUseDetail(itemList);
+			}
+		}
+	}
+
+	private void addUseDetail(List<ItemBenefitUseDetailBo> itemList) {
+		List<ItemBenefitUseDetailBo> useDetail = this.getBenefitUseDetail();
+		if (CollectionUtils.isNotEmpty(useDetail)) {
+			useDetail.addAll(itemList);
+		} else {
+			this.setBenefitUseDetail(itemList);
+		}
+	}
 
 }
--- yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/controller/discount/DiscountController.java
+++ yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/controller/discount/DiscountController.java
@@ -1,19 +1,23 @@
 package com.yunya.middletable.controller.discount;
 
-import com.yunya.feign.middletable.domain.form.PullForm;
-import com.yunya.middletable.service.discount.DiscountServiceImpl;
+import com.yunya.feign.report.domain.form.PullForm;
+import com.yunya.framework.common.model.ResponseResult;
+import com.yunya.framework.common.utils.ResponseUtil;
+import com.yunya.middletable.service.discount.BaseCouponServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DiscountController {
 
     @Autowired
-    DiscountServiceImpl discountService;
+    BaseCouponServiceImpl discountService;
 
-    @RequestMapping("/discount/pull")
-    public String pullData(PullForm form){
-        return null;
+    @PostMapping("/discount/pull")
+    public ResponseResult pullData(@RequestBody PullForm form){
+        discountService.pullCoupon(form.getStartDate(), form.getEndDate());
+        return ResponseUtil.success();
     }
 }
