b5ac7481ba22284bffa9992e85d930b57c21a4ce
zhou_wen <sa52305@outlook.com>
2020-10-16 12:47:38 +0800

    更新消息参数封装

--- yunya-remote/src/main/java/com/yunya/feign/report/domain/model/MessageModel.java
+++ yunya-remote/src/main/java/com/yunya/feign/report/domain/model/MessageModel.java
@@ -17,8 +17,9 @@ import java.util.Map;
 @ToString
 public class MessageModel {
 
-  /** 多个参数用map封装(key1-"id"; key2-"type"),为解决原是表是多张，中间表为一张的数据同步问题 */
+  /** 为解决原是表是多张，中间表为一张的数据同步问题，多个参数用map封装(key1-"id",强制; key2-"type"，建议) */
   private Map<String, Object> paramMap;
+
   /** 操作类型（0-新增 1-修改 2-删除） */
   @ApiModelProperty(value = "操作类型（0-新增 1-修改 2-删除）", required = true)
   @NotNull(message = "消息数据操作类型不能为空！")
