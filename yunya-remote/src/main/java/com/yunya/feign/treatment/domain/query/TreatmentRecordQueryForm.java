91da56935383a8d9daed1134f115c967fce96aa5
zhou_wen <sa52305@outlook.com>
2020-10-20 09:37:48 +0800

    更新就诊列表查询条件

--- yunya-remote/src/main/java/com/yunya/feign/treatment/domain/query/TreatmentRecordQueryForm.java
+++ yunya-remote/src/main/java/com/yunya/feign/treatment/domain/query/TreatmentRecordQueryForm.java
@@ -41,8 +41,7 @@ public class TreatmentRecordQueryForm implements Serializable {
   @NotBlank(message = "查询日期不能为空！")
   private String currentDate;
 
-  @ApiModelProperty(value = "诊疗状态(0-就诊中;1-已开单;2-治疗完成;3-已结账)", required = true)
-  @NotNull(message = "就诊状态不能为空！")
+  @ApiModelProperty(value = "诊疗状态列表(0-就诊中;1-已开单;2-治疗完成;3-已结账)", required = true)
   private Byte[] treatmentStatus;
 
   @ApiModelProperty("接诊ID")
