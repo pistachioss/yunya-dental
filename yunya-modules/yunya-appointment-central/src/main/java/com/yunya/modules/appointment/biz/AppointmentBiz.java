224517ec2d5faba4fb2bed33ec1f675f04b48bd8
lihuibin <951755155@qq.com>
2020-10-21 17:36:45 +0800

    修改预约冲突判断

--- yunya-modules/yunya-appointment-central/src/main/java/com/yunya/modules/appointment/biz/AppointmentBiz.java
+++ yunya-modules/yunya-appointment-central/src/main/java/com/yunya/modules/appointment/biz/AppointmentBiz.java
@@ -973,8 +973,10 @@ public class AppointmentBiz extends BaseBiz<AppointmentMapper, Appointment> {
     private ResponseResult dentistConflictInfo(Integer id, Integer dentistId, Date appointStartTime, Date appointEndTime) {
         List<AppointConflictInfoVo> dentisList;
         if (null != id) {
+            // 添加预约医生冲突检测
             dentisList = mapper.editCheckDentistConflict(id,dentistId,appointStartTime,appointEndTime);
         } else {
+            // 修改预约医生冲突检测
             dentisList = mapper.findAppointListByDentistIdAndAppointStartTimeAndAppointEndTime(
                     dentistId, appointStartTime, appointEndTime);
         }
