9932e8b6bcfdfe84ebaddb6205336438fed3f012
zhou_wen <sa52305@outlook.com>
2020-10-29 10:07:58 +0800

    解决包引入问题

--- yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/dao/report/BaseBenefitMapper.java
+++ yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/dao/report/BaseBenefitMapper.java
@@ -1,6 +1,6 @@
 package com.yunya.middletable.dao.report;
 
-import com.yunya.models.middletable.BaseBenefit;
+import com.yunya.models.report.BaseBenefit;
 import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
