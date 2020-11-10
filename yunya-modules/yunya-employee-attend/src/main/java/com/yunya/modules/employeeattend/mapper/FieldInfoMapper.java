14364359e7f7b12572d373ba5dda0add1bf7a972
chenlin <chenlinpsv@163.com>
2020-11-10 20:46:22 +0800

    增加申请相关的实体和mapper文件

---
+++ yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/mapper/FieldInfoMapper.java
@@ -0,0 +1,7 @@
+package com.yunya.modules.employeeattend.mapper;
+
+import com.yunya.models.employee_attend.FieldInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface FieldInfoMapper extends Mapper<FieldInfo> {
+}
---
+++ yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/mapper/LeaveInfoMapper.java
@@ -0,0 +1,7 @@
+package com.yunya.modules.employeeattend.mapper;
+
+import com.yunya.models.employee_attend.LeaveInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface LeaveInfoMapper extends Mapper<LeaveInfo> {
+}
