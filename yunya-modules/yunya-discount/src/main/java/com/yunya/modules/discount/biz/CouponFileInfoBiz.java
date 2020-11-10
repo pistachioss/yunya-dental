02ee0b4fe3f8d48290843505649c333aed5f8b0e
me <1123038904@qq.com>
2020-11-10 13:15:54 +0800

    文件

--- yunya-modules/yunya-discount/src/main/java/com/yunya/modules/discount/biz/CouponFileInfoBiz.java
+++ yunya-modules/yunya-discount/src/main/java/com/yunya/modules/discount/biz/CouponFileInfoBiz.java
@@ -61,6 +61,7 @@ public class CouponFileInfoBiz extends BaseBiz<CouponFileInfoMapper, CouponFileI
         if (fileForm.getDocs() != null && fileForm.getDocs().size() > 0) {
             for (FileInfo fileInfo : fileForm.getDocs()) {
                 CouponFileInfo couponFileInfo = new CouponFileInfo();
+                couponFileInfo.setFileName(fileInfo.getFileName());
                 couponFileInfo.setCouponId(fileForm.getId());
                 couponFileInfo.setInservice(true);
                 couponFileInfo.setFileType(new Byte("1"));
