c0cba4a26574cffc290c8f1075ecbdfd3d12f592
lihuibin <951755155@qq.com>
2020-11-16 16:24:56 +0800

    修改就诊列表数量统计请求方式

--- yunya-modules/yunya-treatment/src/main/java/com/yunya/modules/treatment/controller/web/TreatmentRecordController.java
+++ yunya-modules/yunya-treatment/src/main/java/com/yunya/modules/treatment/controller/web/TreatmentRecordController.java
@@ -157,7 +157,7 @@ public class TreatmentRecordController {
         dataType = "String",
         paramType = "path")
   })
-  @GetMapping(value = "/count", name = "就诊列表数量统计")
+  @PostMapping(value = "/count", name = "就诊列表数量统计")
   public ResponseResult<Map<String, Integer>> count(@RequestBody TreatmentCountQuery query) {
     Map<String, Integer> resultMap = treatmentRecordBiz.countTreatList(query);
     return ResponseUtil.success(resultMap);
