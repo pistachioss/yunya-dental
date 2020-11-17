99f0c82df21d78596ef1d97d375bd57cb37a4dc1
lihuibin <951755155@qq.com>
2020-11-17 14:34:38 +0800

    add:新增打印信息获取接口

--- yunya-modules/yunya-patient-central/src/main/java/com/yunya/modules/patient_central/controller/web/PrintPatientInfoController.java
+++ yunya-modules/yunya-patient-central/src/main/java/com/yunya/modules/patient_central/controller/web/PrintPatientInfoController.java
@@ -9,10 +9,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.NotNull;
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @program: yunya-dental
