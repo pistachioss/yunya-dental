d8cf219fca2bcb43be6df2523d37fbb6f6939745
lihuibin <951755155@qq.com>
2020-11-24 20:40:40 +0800

    修改账单打印信息

--- yunya-remote/src/main/java/com/yunya/feign/treatment/domain/vo/BillDetailPrintInfoVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/treatment/domain/vo/BillDetailPrintInfoVO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @program: yunya-dental
@@ -16,6 +17,8 @@ import java.math.BigDecimal;
 @ApiModel(value = "BillDetailPrintInfoVO",description = "账单明细打印列表")
 @Data
 public class BillDetailPrintInfoVO implements Serializable {
+    @ApiModelProperty("订单明细ID")
+    private Integer orderDetailId;
     @ApiModelProperty("牙位")
     private String toothBit;
     @ApiModelProperty("数量")
@@ -34,4 +37,7 @@ public class BillDetailPrintInfoVO implements Serializable {
     private BigDecimal actualReceivable;
     @ApiModelProperty("已收金额")
     private BigDecimal receivedAmount;
+    /** 卡券类型 */
+    @ApiModelProperty(value = "优惠券类型（0：代金券 1：折扣券 2：兑换券 3：套餐券 99：会员卡 5：授权折扣）")
+    private List<Integer> couponTypes;
 }
