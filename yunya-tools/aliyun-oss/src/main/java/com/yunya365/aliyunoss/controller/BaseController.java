7cfa696078e7967c197ddfc7964393cc7d8f0cb4
ChordKey <chtitaxie@hotmail.com>
2020-11-26 17:36:06 +0800

    add: oss category, attendence.

--- yunya-tools/aliyun-oss/src/main/java/com/yunya365/aliyunoss/controller/BaseController.java
+++ yunya-tools/aliyun-oss/src/main/java/com/yunya365/aliyunoss/controller/BaseController.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 
 
 @RestController
-@Api(tags = "资源相关接口，oss_category分为1:诊所,2:员工,3:患者,4:优惠活动，其它将放入0:临时文件夹")
+@Api(tags = "oss_category说明,1:诊所,2:员工,3:患者,4:优惠活动,5:考勤,0:临时文件夹.")
 public class BaseController {
 
     private String getSuffixName(final String fileName) {
