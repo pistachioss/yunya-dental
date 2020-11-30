88ff0733c8abe00b381472060fc0fa300b66df55
me <1123038904@qq.com>
2020-11-30 16:25:18 +0800

    考勤

--- yunya-remote/src/main/java/com/yunya/feign/employee_attend/vo/LeaveInfoListVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/employee_attend/vo/LeaveInfoListVO.java
@@ -90,5 +90,7 @@ public class LeaveInfoListVO {
     @ApiModelProperty(value = "创建时间")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date crtTime;
+    @ApiModelProperty("请假时长(分钟)")
+    private Integer leaveTime;
 
 }
