2c6f3dc039801eb5bdc597c430f31c5570259b6c
me <1123038904@qq.com>
2020-12-02 17:25:36 +0800

    考勤

--- yunya-remote/src/main/java/com/yunya/feign/employee_attend/vo/FieldInfoListVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/employee_attend/vo/FieldInfoListVO.java
@@ -76,4 +76,8 @@ public class FieldInfoListVO {
     @ApiModelProperty(value = "创建时间")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date crtTime;
+
+    @ApiModelProperty(value = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updTime;
 }
