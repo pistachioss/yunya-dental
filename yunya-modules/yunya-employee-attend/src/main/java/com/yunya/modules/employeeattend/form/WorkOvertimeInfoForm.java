eca35e36bc241f1faaaba567204c7c6df0ef50a2
me <1123038904@qq.com>
2020-12-03 16:57:13 +0800

    考勤

--- yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/form/WorkOvertimeInfoForm.java
+++ yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/form/WorkOvertimeInfoForm.java
@@ -32,6 +32,14 @@ public class WorkOvertimeInfoForm {
 
     @ApiModelProperty("申请要加班的日期")
     private Date workDate;
+
+    @ApiModelProperty("开始时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date startTime;
+
+    @ApiModelProperty("结束时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date endTime;
     /**
      * 门诊id
      */
