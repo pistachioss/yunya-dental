5968119266b15f39af7de0bb51b53d499e90e02d
me <1123038904@qq.com>
2020-12-03 09:54:43 +0800

    考勤

--- yunya-remote/src/main/java/com/yunya/feign/employee_attend/vo/LeaveInfoListVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/employee_attend/vo/LeaveInfoListVO.java
@@ -32,6 +32,10 @@ public class LeaveInfoListVO {
      */
     @ApiModelProperty(value = "申请人id")
     private Integer userId;
+
+
+    @ApiModelProperty(value = "假期类型 0：班次 1：天")
+    private Integer vacationStatus;
     /**
      * 申请人
      */
