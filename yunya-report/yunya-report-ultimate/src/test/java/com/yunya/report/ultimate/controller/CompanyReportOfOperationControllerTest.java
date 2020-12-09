b14e6a3af4c5c8cd12b4bf3e61aaf2da3b9f1230
zhou_wen <sa52305@outlook.com>
2020-12-09 17:45:49 +0800

    新增查询运营报表-数据总览单元测试

--- yunya-report/yunya-report-ultimate/src/test/java/com/yunya/report/ultimate/controller/CompanyReportOfOperationControllerTest.java
+++ yunya-report/yunya-report-ultimate/src/test/java/com/yunya/report/ultimate/controller/CompanyReportOfOperationControllerTest.java
@@ -1,6 +1,8 @@
 package com.yunya.report.ultimate.controller;
 
 import com.github.pagehelper.PageInfo;
+import com.yunya.feign.report.domain.bo.ClinicDataStatisticsInfoVO;
+import com.yunya.feign.report.domain.query.DataStatisticsQuery;
 import com.yunya.feign.report.domain.query.EmployeeWorkloadQuery;
 import com.yunya.feign.report.domain.vo.EmployeeWorkloadOfOperationVO;
 import com.yunya.framework.common.model.ResponseResult;
@@ -30,9 +32,21 @@ public class CompanyReportOfOperationControllerTest {
     query.setOrgId(35);
     query.setDateType((byte) 0);
     query.setQueryDate("2020-12");
-    query.setEmployeeIds(new Integer[]{526});
+    query.setEmployeeIds(new Integer[] {526});
     ResponseResult<PageInfo<EmployeeWorkloadOfOperationVO>> result =
         companyReportOfOperationController.employeeWorkloadListOfOperation(query);
+    System.out.println(result);
+  }
+
+  @Test
+  public void find() {
+    DataStatisticsQuery query = new DataStatisticsQuery();
+    query.setOrgIds(new Integer[] {35, 42, 72});
+    query.setDateType((byte) 0);
+    query.setStartDate("2020-07-01");
+    query.setEndDate("2020-12-12");
+    ResponseResult<ClinicDataStatisticsInfoVO> result =
+        companyReportOfOperationController.operationalAnalysisDataStatistics(query);
     System.out.println(result);
   }
 }
