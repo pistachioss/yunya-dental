e7c719e8aa6d228ae28799a0351dae6dd6c588be
me <1123038904@qq.com>
2020-12-15 17:13:59 +0800

    考勤

--- yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/biz/FieldInfoBiz.java
+++ yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/biz/FieldInfoBiz.java
@@ -334,10 +334,15 @@ public class FieldInfoBiz extends BaseBiz<FieldInfoMapper, FieldInfo> {
             for (ApprovalAllListVO approvalAllListVO : reList) {
                 approvalAllListVO.setUserName(emMapById.get(approvalAllListVO.getUserId() + "").getName());
                 String approvalName = "";
-                if(approvalAllListVO.getApprovalPeopleId()!=null){
+                if (approvalAllListVO.getApprovalPeopleId() != null) {
                     String[] split = approvalAllListVO.getApprovalPeopleId().split(",");
                     for (int i = 0; i < split.length; i++) {
-                        approvalName = emMapById.get(split[i]).getName() + ",";
+                        if (i == 0) {
+                            approvalName = emMapById.get(split[i]).getName();
+                        } else {
+                            approvalName = approvalName + "," + emMapById.get(split[i]).getName();
+                        }
+
                     }
                 }
                 approvalAllListVO.setApprovalPeopleName(approvalName);
