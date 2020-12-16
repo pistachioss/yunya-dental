647072449e67caabe51662517cda34612f588272
chenlin <chenlinpsv@163.com>
2020-12-16 16:45:15 +0800

    修复bug：考勤月汇总无效卡统计为空

--- yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/biz/AttendancePunchRecordBiz.java
+++ yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/biz/AttendancePunchRecordBiz.java
@@ -817,7 +817,6 @@ public class AttendancePunchRecordBiz extends BaseBiz<AttendancePunchRecordMappe
         Date now = new Date(System.currentTimeMillis());
         AttendanceStatisticsVO result = new AttendanceStatisticsVO();
         Integer userId = Integer.parseInt(BaseContextHandler.getUserID());
-        userId = 561;
         List<Date> dateList = DateUtil.getMonthFullDay(dateStr);
         Date firstDate = dateList.get(0);
         Date endDate = dateList.get(dateList.size()-1);
