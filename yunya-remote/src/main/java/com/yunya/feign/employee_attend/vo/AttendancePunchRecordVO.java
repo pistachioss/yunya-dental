e776b67fa3fa1005931ab3c97b45e3568ee9f6a2
chenlin <chenlinpsv@163.com>
2020-12-16 10:15:53 +0800

    修复bug：考勤汇总报错

--- yunya-remote/src/main/java/com/yunya/feign/employee_attend/vo/AttendancePunchRecordVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/employee_attend/vo/AttendancePunchRecordVO.java
@@ -112,10 +112,6 @@ public class AttendancePunchRecordVO implements Serializable {
     @ApiModelProperty(value = "wifi的mac地址")
     private String wifiMacAddress;
 
-    /** 排班班次id */
-    @ApiModelProperty(value = "排班班次id")
-    private Integer scheduleId;
-
     /** 时长（小于60分钟的只显示分钟，否则显示xx小时xx分钟） */
     @ApiModelProperty(value = "时长（分钟）")
     private String minutes;
