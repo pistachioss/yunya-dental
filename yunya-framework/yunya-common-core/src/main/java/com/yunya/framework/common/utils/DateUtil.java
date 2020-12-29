d15a9e78690c814bf49ec1e07070f11df4e4f9ed
zhou_wen <sa52305@outlook.com>
2020-12-29 14:54:32 +0800

    优化代码

--- yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/utils/DateUtil.java
+++ yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/utils/DateUtil.java
@@ -25,7 +25,6 @@ public class DateUtil {
   private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
 
   private DateUtil() {}
-  ;
 
   public static Date geLastWeekMonday(Date date) {
     Calendar cal = Calendar.getInstance();
@@ -86,11 +85,7 @@ public class DateUtil {
     Calendar end = Calendar.getInstance();
     end.setTime(endTime);
 
-    if (date.after(begin) && date.before(end)) {
-      return true;
-    } else {
-      return false;
-    }
+    return date.after(begin) && date.before(end);
   }
 
   public static LocalDateTime dateToLocalDateTime(Date date) {
@@ -278,14 +273,14 @@ public class DateUtil {
     }
 
     Integer count = list.size(); // 记录总数
-    Integer pageCount = 0; // 页数
+    int pageCount; // 页数
     if (count % pageSize == 0) {
       pageCount = count / pageSize;
     } else {
       pageCount = count / pageSize + 1;
     }
-    int fromIndex = 0; // 开始索引
-    int toIndex = 0; // 结束索引
+    int fromIndex; // 开始索引
+    int toIndex; // 结束索引
     if (!pageNum.equals(pageCount)) {
       fromIndex = (pageNum - 1) * pageSize;
       toIndex = fromIndex + pageSize;
