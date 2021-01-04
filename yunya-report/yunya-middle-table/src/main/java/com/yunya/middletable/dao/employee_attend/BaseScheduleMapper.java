01f1a31991eb4cac2e8b52cdd3b4595e074d11d6
me <1123038904@qq.com>
2021-01-04 14:32:26 +0800

    排班消息处理

---
+++ yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/dao/employee_attend/BaseScheduleMapper.java
@@ -0,0 +1,10 @@
+package com.yunya.middletable.dao.employee_attend;
+
+
+
+
+import com.yunya.models.employee_attend.BaseSchedule;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BaseScheduleMapper extends Mapper<BaseSchedule> {
+}
