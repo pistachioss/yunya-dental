f2e10d44eaca1b38295b78613061656076eca6ca
zhou_wen <sa52305@outlook.com>
2021-01-13 16:54:41 +0800

    更新对账单账单收费导出

--- yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/biz/BaseBillPayBiz.java
+++ yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/biz/BaseBillPayBiz.java
@@ -5,10 +5,7 @@ import com.github.pagehelper.PageInfo;
 import com.yunya.feign.report.domain.query.BillPayRecordQuery;
 import com.yunya.feign.report.domain.query.DataStatisticsQuery;
 import com.yunya.feign.report.domain.query.StatementBillChargeDetailInfoQuery;
-import com.yunya.feign.report.domain.vo.BillOfPayRecordVO;
-import com.yunya.feign.report.domain.vo.StatementBillChargeDetailVO;
-import com.yunya.feign.report.domain.vo.StatementPaymentVO;
-import com.yunya.feign.report.domain.vo.TollDataStatisticsVO;
+import com.yunya.feign.report.domain.vo.*;
 import com.yunya.framework.common.biz.BaseBiz;
 import com.yunya.framework.common.utils.StringHelper;
 import com.yunya.framework.common.utils.poi.ExcelUtil;
@@ -17,12 +14,14 @@ import com.yunya.models.report.BaseOrganization;
 import com.yunya.report.ultimate.mapper.BaseBillPayDetailMapper;
 import com.yunya.report.ultimate.mapper.BaseBillPayMapper;
 import com.yunya.report.ultimate.mapper.BaseOrganizationMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -100,6 +99,32 @@ public class BaseBillPayBiz extends BaseBiz<BaseBillPayMapper, BaseBillPay> {
   }
 
   /**
+   * 根据条件导出门诊账单收费（本月）明细列表
+   *
+   * @param response http响应
+   * @param query 查询条件
+   */
+  public void exportBillChargeDetailInfoList(
+      HttpServletResponse response, StatementBillChargeDetailInfoQuery query) throws IOException {
+    PageInfo<StatementBillChargeDetailVO> pageInfo = findBillChargeDetailInfoList(query);
+    List<StatementBillChargeDetailVO> resultList = pageInfo.getList();
+    ExcelUtil<StatementBillChargeDetailExportVO> excelUtil =
+        new ExcelUtil<>(StatementBillChargeDetailExportVO.class);
+    // 构建导出数据列表
+    List<StatementBillChargeDetailExportVO> exportList =
+        buildStatementBillChargeDetailExportList(resultList);
+    String fileName = "门诊账单收费（本月）明细列表";
+    BaseOrganization organization = organizationMapper.selectByPrimaryKey(query.getOrgId());
+    if (null != organization) {
+      fileName =
+          MessageFormat.format(
+              "{0}{1}-{2}{3}",
+              organization.getAbbreviation(), query.getStartDate(), query.getEndDate(), fileName);
+    }
+    excelUtil.exportExcel(response, exportList, "门诊账单收费（本月）明细", fileName);
+  }
+
+  /**
    * 根据条件查询账单收欠费（本月账单本月收费）详情信息列表
    *
    * @param query 查询条件
@@ -114,6 +139,32 @@ public class BaseBillPayBiz extends BaseBiz<BaseBillPayMapper, BaseBillPay> {
         mapper.selectBillCurrentChargeDebtDetailList(query);
     generateBillChargeAccountItemDetail(resultList);
     return new PageInfo<>(resultList);
+  }
+
+  /**
+   * 根据条件导出账单收欠费（本月账单本月收费）详情信息列表
+   *
+   * @param response 响应
+   * @param query 查询条件
+   */
+  public void exportBillCollectDebtDetailList(
+      HttpServletResponse response, StatementBillChargeDetailInfoQuery query) throws IOException {
+    PageInfo<StatementBillChargeDetailVO> pageInfo = findBillCurrentCollectDebtDetailList(query);
+    List<StatementBillChargeDetailVO> resultList = pageInfo.getList();
+    ExcelUtil<StatementBillChargeDetailExportVO> excelUtil =
+        new ExcelUtil<>(StatementBillChargeDetailExportVO.class);
+    // 构建导出数据列表
+    List<StatementBillChargeDetailExportVO> exportList =
+        buildStatementBillChargeDetailExportList(resultList);
+    String fileName = "门诊账单收欠费（本月）明细列表";
+    BaseOrganization organization = organizationMapper.selectByPrimaryKey(query.getOrgId());
+    if (null != organization) {
+      fileName =
+          MessageFormat.format(
+              "{0}{1}-{2}{3}",
+              organization.getAbbreviation(), query.getStartDate(), query.getEndDate(), fileName);
+    }
+    excelUtil.exportExcel(response, exportList, "门诊账单收欠费（本月）明细", fileName);
   }
 
   /**
@@ -134,7 +185,33 @@ public class BaseBillPayBiz extends BaseBiz<BaseBillPayMapper, BaseBillPay> {
   }
 
   /**
-   * 根据条件查询门诊账单代收详情信息列表 todo:补充会员卡本金、会员卡赠金；预付款本金，预付款赠金支付方式
+   * 根据条件导出账单收欠费（非本月账单本月收费）详情信息列表
+   *
+   * @param response http响应
+   * @param query 查询条件
+   */
+  public void exportBillOtherCollectDebtDetailList(
+      HttpServletResponse response, StatementBillChargeDetailInfoQuery query) throws IOException {
+    PageInfo<StatementBillChargeDetailVO> pageInfo = findOtherCollectDebtDetailList(query);
+    List<StatementBillChargeDetailVO> resultList = pageInfo.getList();
+    ExcelUtil<StatementBillChargeDetailExportVO> excelUtil =
+        new ExcelUtil<>(StatementBillChargeDetailExportVO.class);
+    // 构建导出数据列表
+    List<StatementBillChargeDetailExportVO> exportList =
+        buildStatementBillChargeDetailExportList(resultList);
+    String fileName = "门诊账单收欠费（非本月）明细列表";
+    BaseOrganization organization = organizationMapper.selectByPrimaryKey(query.getOrgId());
+    if (null != organization) {
+      fileName =
+          MessageFormat.format(
+              "{0}{1}-{2}{3}",
+              organization.getAbbreviation(), query.getStartDate(), query.getEndDate(), fileName);
+    }
+    excelUtil.exportExcel(response, exportList, "门诊账单收欠费（非本月）明细", fileName);
+  }
+
+  /**
+   * 根据条件查询门诊账单代收详情信息列表
    *
    * @param query 查询条件
    * @return
@@ -160,19 +237,22 @@ public class BaseBillPayBiz extends BaseBiz<BaseBillPayMapper, BaseBillPay> {
       HttpServletResponse response, StatementBillChargeDetailInfoQuery query) throws IOException {
     PageInfo<StatementBillChargeDetailVO> pageInfo = findCurrentBillCollectionDetailList(query);
     List<StatementBillChargeDetailVO> list = pageInfo.getList();
-    ExcelUtil<StatementBillChargeDetailVO> excelUtil =
-        new ExcelUtil<>(StatementBillChargeDetailVO.class);
+    // 构建导出数据列表
+    List<StatementBillChargeDetailExportVO> exportList =
+        buildStatementBillChargeDetailExportList(list);
+    ExcelUtil<StatementBillChargeDetailExportVO> excelUtil =
+        new ExcelUtil<>(StatementBillChargeDetailExportVO.class);
     BaseOrganization organization = organizationMapper.selectByPrimaryKey(query.getOrgId());
-    String fileName = "诊所代收(本月)记录明细";
+    String fileName = "诊所代收(本月)记录明细列表";
     if (null != organization) {
       String abbreviation = organization.getAbbreviation();
       fileName = abbreviation + fileName;
     }
-    excelUtil.exportExcel(response, list, "诊所代收(本月)记录明细列表", fileName);
+    excelUtil.exportExcel(response, exportList, "诊所代收(本月)记录明细", fileName);
   }
 
   /**
-   * 根据条件查询门诊账单代（非本月）收详情信息列表 todo:补充会员卡本金、会员卡赠金；预付款本金，预付款赠金支付方式
+   * 根据条件查询门诊账单代（非本月）收详情信息列表
    *
    * @param query 查询条件
    * @return PageInfo<StatementBillChargeDetailVO>
@@ -195,7 +275,22 @@ public class BaseBillPayBiz extends BaseBiz<BaseBillPayMapper, BaseBillPay> {
    * @param query 查询条件
    */
   public void exportOtherBillCollectionDetailList(
-      HttpServletResponse response, StatementBillChargeDetailInfoQuery query) {}
+      HttpServletResponse response, StatementBillChargeDetailInfoQuery query) throws IOException {
+    PageInfo<StatementBillChargeDetailVO> pageInfo = findOtherBillCollectionDetailList(query);
+    List<StatementBillChargeDetailVO> list = pageInfo.getList();
+    // 构建导出数据列表
+    List<StatementBillChargeDetailExportVO> exportList =
+        buildStatementBillChargeDetailExportList(list);
+    ExcelUtil<StatementBillChargeDetailExportVO> excelUtil =
+        new ExcelUtil<>(StatementBillChargeDetailExportVO.class);
+    BaseOrganization organization = organizationMapper.selectByPrimaryKey(query.getOrgId());
+    String fileName = "诊所代收(非本月)记录明细列表";
+    if (null != organization) {
+      String abbreviation = organization.getAbbreviation();
+      fileName = abbreviation + fileName;
+    }
+    excelUtil.exportExcel(response, exportList, "诊所代收(非本月)记录明细", fileName);
+  }
 
   /**
    * 根据条件查询诊所被代收账（本月）明细列表
@@ -221,7 +316,22 @@ public class BaseBillPayBiz extends BaseBiz<BaseBillPayMapper, BaseBillPay> {
    * @param query 查询条件
    */
   public void exportCurrentBillIsAcceptedDetailList(
-      HttpServletResponse response, StatementBillChargeDetailInfoQuery query) {}
+      HttpServletResponse response, StatementBillChargeDetailInfoQuery query) throws IOException {
+    PageInfo<StatementBillChargeDetailVO> pageInfo = findCurrentBillIsAcceptedDetailList(query);
+    List<StatementBillChargeDetailVO> list = pageInfo.getList();
+    // 构建导出数据列表
+    List<StatementBillChargeDetailExportVO> exportList =
+        buildStatementBillChargeDetailExportList(list);
+    ExcelUtil<StatementBillChargeDetailExportVO> excelUtil =
+        new ExcelUtil<>(StatementBillChargeDetailExportVO.class);
+    BaseOrganization organization = organizationMapper.selectByPrimaryKey(query.getOrgId());
+    String fileName = "诊所被代收账(本月)记录明细列表";
+    if (null != organization) {
+      String abbreviation = organization.getAbbreviation();
+      fileName = abbreviation + fileName;
+    }
+    excelUtil.exportExcel(response, exportList, "诊所被代收账(本月)记录明细", fileName);
+  }
 
   /**
    * 根据条件查询诊所被代收账（非本月）明细列表
@@ -247,7 +357,22 @@ public class BaseBillPayBiz extends BaseBiz<BaseBillPayMapper, BaseBillPay> {
    * @param query 查询条件
    */
   public void exportOtherBillIsAcceptedDetailList(
-      HttpServletResponse response, StatementBillChargeDetailInfoQuery query) {}
+      HttpServletResponse response, StatementBillChargeDetailInfoQuery query) throws IOException {
+    PageInfo<StatementBillChargeDetailVO> pageInfo = findOtherBillIsAcceptedDetailList(query);
+    List<StatementBillChargeDetailVO> list = pageInfo.getList();
+    // 构建导出数据列表
+    List<StatementBillChargeDetailExportVO> exportList =
+        buildStatementBillChargeDetailExportList(list);
+    ExcelUtil<StatementBillChargeDetailExportVO> excelUtil =
+        new ExcelUtil<>(StatementBillChargeDetailExportVO.class);
+    BaseOrganization organization = organizationMapper.selectByPrimaryKey(query.getOrgId());
+    String fileName = "诊所被代收账(非本月)记录明细列表";
+    if (null != organization) {
+      String abbreviation = organization.getAbbreviation();
+      fileName = abbreviation + fileName;
+    }
+    excelUtil.exportExcel(response, exportList, "诊所被代收账(非本月)记录明细", fileName);
+  }
 
   /**
    * 构建账单收费记录的支付方式明细信息
@@ -304,5 +429,85 @@ public class BaseBillPayBiz extends BaseBiz<BaseBillPayMapper, BaseBillPay> {
         vo.setStatementPayments(statementPaymentResult);
       }
     }
+  }
+
+  /**
+   * 构建对账单账单收费明细导出列表
+   *
+   * @param resultList 账单收费明细列表
+   * @return List<StatementBillChargeDetailExportVO>
+   */
+  private List<StatementBillChargeDetailExportVO> buildStatementBillChargeDetailExportList(
+      List<StatementBillChargeDetailVO> resultList) {
+    ArrayList<StatementBillChargeDetailExportVO> chargeDetailExports = new ArrayList<>();
+    if (StringHelper.isNotEmpty(resultList)) {
+      for (StatementBillChargeDetailVO vo : resultList) {
+        StatementBillChargeDetailExportVO chargeDetailExport =
+            new StatementBillChargeDetailExportVO();
+        BeanUtils.copyProperties(vo, chargeDetailExport);
+        List<StatementPaymentVO> payments = vo.getStatementPayments();
+        if (StringHelper.isNotEmpty(payments)) {
+          for (StatementPaymentVO payment : payments) {
+            // todo 当前硬编码全部支付方式，后期再考虑根据现有支付方式动态设置
+            String accountItemName = payment.getAccountItemName();
+            BigDecimal totalAmount = payment.getTotalAmount();
+            switch (accountItemName) {
+              case "会员卡本金":
+                chargeDetailExport.setMemberPrincipleAmount(totalAmount);
+                break;
+              case "会员卡赠金":
+                chargeDetailExport.setMemberBonusAmount(totalAmount);
+                break;
+              case "预付款本金":
+                chargeDetailExport.setPrepaidPrincipleAmount(totalAmount);
+                break;
+              case "预付款赠金":
+                chargeDetailExport.setPrepaidBonusAmount(totalAmount);
+                break;
+              case "现金":
+                chargeDetailExport.setCashAmount(totalAmount);
+                break;
+              case "支付宝":
+                chargeDetailExport.setAliPayAmount(totalAmount);
+                break;
+              case "微信":
+                chargeDetailExport.setWeChatAmount(totalAmount);
+                break;
+              case "银行账户":
+                chargeDetailExport.setBankAmount(totalAmount);
+                break;
+              case "浙江省医保":
+                chargeDetailExport.setZheJiangProvinceMedicalInsuranceAmount(totalAmount);
+                break;
+              case "杭州市医保":
+                chargeDetailExport.setHangZhouCityMedicalInsuranceAmount(totalAmount);
+                break;
+              case "杭州市余杭医保":
+                chargeDetailExport.setHangZhouYuHangMedicalInsuranceAmount(totalAmount);
+                break;
+              case "杭州市萧山医保":
+                chargeDetailExport.setHangZhouXiaoShanMedicalInsuranceAmount(totalAmount);
+                break;
+              case "Cigna":
+                chargeDetailExport.setCignaAmount(totalAmount);
+                break;
+              case "MSH":
+                chargeDetailExport.setMshAmount(totalAmount);
+                break;
+              case "AXA":
+                chargeDetailExport.setAxaAmount(totalAmount);
+                break;
+              case "风石":
+                chargeDetailExport.setFengShiAmount(totalAmount);
+                break;
+              default:
+                break;
+            }
+          }
+        }
+        chargeDetailExports.add(chargeDetailExport);
+      }
+    }
+    return chargeDetailExports;
   }
 }
