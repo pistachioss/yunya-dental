34ff6eaf6988aa3b22ea62706066adc08aa7b734
ChordKey <chtitaxie@hotmail.com>
2021-01-14 15:49:35 +0800

    fix: report export response status 0 when null

--- yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/controller/DiscountController.java
+++ yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/controller/DiscountController.java
@@ -19,6 +19,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author xiangyang
@@ -123,88 +124,128 @@ public class DiscountController {
 
 	@ApiOperation(value = "产品记录-产品售出记录 - 导出")
 	@PostMapping("/coupon/sold/record/export")
-	public void exportCouponSold(HttpServletResponse response, @Valid @RequestBody CardSoldRecordQuery query) throws IOException {
-		discountBiz.buildResponse(response, discountBiz.getOrgName(query.getOrgId()) + "产品售出记录表");
-		EasyExcel.write(response.getOutputStream(), RechargeCardStatisticsVo.class)
-				.sheet("sheet").doWrite(discountBiz.getCardSoldRecordList(query));
+	public ResponseResult exportCouponSold(HttpServletResponse response, @Valid @RequestBody CardSoldRecordQuery query) throws IOException {
+		List<CouponSoldRecordVo> list = discountBiz.getCardSoldRecordList(query);
+		if(list != null && !list.isEmpty()){
+			discountBiz.buildResponse(response, discountBiz.getOrgName(query.getOrgId()) + "产品售出记录表");
+			EasyExcel.write(response.getOutputStream(), RechargeCardStatisticsVo.class)
+					.sheet("sheet").doWrite(discountBiz.getCardSoldRecordList(query));
+		}
+		return ResponseUtil.success();
 	}
 
 	@ApiOperation(value = "产品记录-产品使用记录 - 导出")
 	@PostMapping("/coupon/used/record/export")
-	public void exportCouponUsedRecord(HttpServletResponse response, @Valid @RequestBody CardUsedRecordQuery query) throws IOException {
-		discountBiz.buildResponse(response, discountBiz.getOrgName(query.getOrgId()) + "产品使用记录表");
-		EasyExcel.write(response.getOutputStream(), CardSoldStatisticsVo.class)
-				.sheet("sheet").doWrite(discountBiz.getCardUsedRecordList(query));
+	public ResponseResult exportCouponUsedRecord(HttpServletResponse response, @Valid @RequestBody CardUsedRecordQuery query) throws IOException {
+		List<CardUsedRecordVo> list = discountBiz.getCardUsedRecordList(query);
+		if(list != null && !list.isEmpty()){
+			discountBiz.buildResponse(response, discountBiz.getOrgName(query.getOrgId()) + "产品使用记录表");
+			EasyExcel.write(response.getOutputStream(), CardSoldStatisticsVo.class)
+					.sheet("sheet").doWrite(discountBiz.getCardUsedRecordList(query));
+		}
+		return ResponseUtil.success();
 	}
 
 	@ApiOperation(value = "产品售出激活统计-售出激活统计（代金、折扣、兑换、套餐）- 导出")
 	@PostMapping("/{couponId}/card/export")
-	public void exportCard(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
+	public ResponseResult exportCard(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
 	                       @Valid @RequestBody CardStatisticsQuery query) throws IOException {
-		discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "产品使用记录表");
-		EasyExcel.write(response.getOutputStream(), CardStatisticsVo.class)
-				.sheet("sheet").doWrite(discountBiz.getCardStatisticsList(couponId, query));
+		List<CardStatisticsVo> list = discountBiz.getCardStatisticsList(couponId, query);
+		if(list != null && !list.isEmpty()){
+			discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "产品使用记录表");
+			EasyExcel.write(response.getOutputStream(), CardStatisticsVo.class)
+					.sheet("sheet").doWrite(list);
+		}
+		return ResponseUtil.success();
 	}
 
 	@ApiOperation(value = "产品售出激活统计 - 售出激活统计（充值卡）- 导出")
 	@PostMapping("/{couponId}/rechargeCard/export")
-	public void exportRechargeCard(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
+	public ResponseResult exportRechargeCard(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
 	                               @Valid @RequestBody RechargeCardStatisticsQuery query) throws IOException {
-		discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "售出激活统计");
-		EasyExcel.write(response.getOutputStream(), RechargeCardStatisticsVo.class)
-				.sheet("sheet").doWrite(discountBiz.getRechargeCardStatisticsList(couponId, query));
+		List<RechargeCardStatisticsVo> list = discountBiz.getRechargeCardStatisticsList(couponId, query);
+		if(list != null && !list.isEmpty()){
+			discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "售出激活统计");
+			EasyExcel.write(response.getOutputStream(), RechargeCardStatisticsVo.class)
+					.sheet("sheet").doWrite(discountBiz.getRechargeCardStatisticsList(couponId, query));
+		}
+		return ResponseUtil.success();
 	}
 
 	@ApiOperation(value = "产品售出统计-时间维度 - 导出")
 	@PostMapping("/card/sold/export")
-	public void exportCardSold(HttpServletResponse response, @Valid @RequestBody CardSoldStatisticsQuery query) throws IOException {
-		discountBiz.buildResponse(response, "自有平台卡券售出明细");
-		EasyExcel.write(response.getOutputStream(), CardSoldStatisticsVo.class)
-				.sheet("sheet").doWrite(discountBiz.getCardSoldList(query));
+	public ResponseResult exportCardSold(HttpServletResponse response, @Valid @RequestBody CardSoldStatisticsQuery query) throws IOException {
+		List<CardSoldStatisticsVo> list = discountBiz.getCardSoldList(query);
+		if(list != null && !list.isEmpty()){
+			discountBiz.buildResponse(response, "自有平台卡券售出明细");
+			EasyExcel.write(response.getOutputStream(), CardSoldStatisticsVo.class)
+					.sheet("sheet").doWrite(discountBiz.getCardSoldList(query));
+		}
+		return ResponseUtil.success();
 	}
 
 	@ApiOperation(value = "产品售出统计-自有平台卡券售出明细 - 导出")
 	@PostMapping("/{couponId}/card/sold/export")
-	public void exportCouponSoldStatistics(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
+	public ResponseResult exportCouponSoldStatistics(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
 	                                       @Valid @RequestBody CouponSoldDetailQuery query) throws IOException {
-		discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "自有平台卡券售出明细");
-		EasyExcel.write(response.getOutputStream(), CouponSoldDetailVo.class)
-				.sheet("sheet").doWrite(discountBiz.getCouponSoldDetailList(couponId, query));
+		List<CouponSoldDetailVo> list = discountBiz.getCouponSoldDetailList(couponId, query);
+		if(list != null && !list.isEmpty()){
+			discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "自有平台卡券售出明细");
+			EasyExcel.write(response.getOutputStream(), CouponSoldDetailVo.class)
+					.sheet("sheet").doWrite(discountBiz.getCouponSoldDetailList(couponId, query));
+		}
+		return ResponseUtil.success();
 	}
 
 	@ApiOperation(value = "产品售出统计-第三方平台卡券激活 - 导出")
 	@PostMapping("/{couponId}/card/active/export")
-	public void exportCouponActiveStatistics(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
+	public ResponseResult exportCouponActiveStatistics(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
 	                                         @Valid @RequestBody CouponActiveDetailQuery query) throws IOException {
-		discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "第三方平台卡券售出明细");
-		EasyExcel.write(response.getOutputStream(), CouponActiveDetailVo.class)
-				.sheet("sheet").doWrite(discountBiz.getCouponActiveList(couponId, query));
+		List<CouponActiveDetailVo> list = discountBiz.getCouponActiveList(couponId, query);
+		if(list != null && !list.isEmpty()){
+			discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "第三方平台卡券售出明细");
+			EasyExcel.write(response.getOutputStream(), CouponActiveDetailVo.class)
+					.sheet("sheet").doWrite(discountBiz.getCouponActiveList(couponId, query));
+		}
+		return ResponseUtil.success();
 	}
 
 	@ApiOperation(value = "产品使用统计-时间维度 - 导出")
 	@PostMapping("/card/used/export")
-	public void exportCouponUsed(HttpServletResponse response, @Valid @RequestBody CardUsedStatisticsQuery query) throws IOException {
-		discountBiz.buildResponse(response, "产品使用统计明细");
-		EasyExcel.write(response.getOutputStream(), CardUsedStatisticsVo.class)
-				.sheet("sheet").doWrite(discountBiz.getCardUsedList(query));
+	public ResponseResult exportCouponUsed(HttpServletResponse response, @Valid @RequestBody CardUsedStatisticsQuery query) throws IOException {
+		List<CardUsedStatisticsVo> list = discountBiz.getCardUsedList(query);
+		if(list != null && !list.isEmpty()){
+			discountBiz.buildResponse(response, "产品使用统计明细");
+			EasyExcel.write(response.getOutputStream(), CardUsedStatisticsVo.class)
+					.sheet("sheet").doWrite(discountBiz.getCardUsedList(query));
+		}
+		return ResponseUtil.success();
 	}
 
 	@ApiOperation(value = "产品使用统计-产品维度-使用统计 - 导出")
 	@PostMapping("/{couponId}/card/used/export")
-	public void exportCouponUsed(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
+	public ResponseResult exportCouponUsed(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
 	                             @Valid @RequestBody CouponUsedDetailQuery query) throws IOException {
-		discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "产品使用统计明细");
-		EasyExcel.write(response.getOutputStream(), CouponUsedDetailVo.class)
-				.sheet("sheet").doWrite(discountBiz.getCouponDetailUsedList(couponId, query));
+		List<CouponUsedDetailVo> list = discountBiz.getCouponDetailUsedList(couponId, query);
+		if(list != null && !list.isEmpty()){
+			discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "产品使用统计明细");
+			EasyExcel.write(response.getOutputStream(), CouponUsedDetailVo.class)
+					.sheet("sheet").doWrite(discountBiz.getCouponDetailUsedList(couponId, query));
+		}
+		return ResponseUtil.success();
 	}
 
 	@ApiOperation(value = "充值卡充值统计-充值统计 - 导出")
 	@PostMapping("/{couponId}/recharge/export")
-	public void getRechargePage(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
+	public ResponseResult getRechargePage(HttpServletResponse response, @PathVariable(value = "couponId") Integer couponId,
 	                            @Valid @RequestBody RechargeDetailQuery query) throws IOException {
-		discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "充值卡充值统计明细");
-		EasyExcel.write(response.getOutputStream(), RechargeDetailVo.class)
-				.sheet("sheet").doWrite(discountBiz.getRechargeDetailList(couponId, query));
+		List<RechargeDetailVo> list = discountBiz.getRechargeDetailList(couponId, query);
+		if(list != null && !list.isEmpty()){
+			discountBiz.buildResponse(response, discountBiz.getCouponName(couponId) + "充值卡充值统计明细");
+			EasyExcel.write(response.getOutputStream(), RechargeDetailVo.class)
+					.sheet("sheet").doWrite(discountBiz.getRechargeDetailList(couponId, query));
+		}
+		return ResponseUtil.success();
 	}
 
 	@ApiOperation(value = "患者档案-产品管理-使用记录（代金券，折扣券）")
