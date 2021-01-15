e99827a0da35efa3afe4b6c8f0ce242cb6d82019
chenlin <chenlinpsv@163.com>
2021-01-15 13:08:13 +0800

    修复bug

---
+++ yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/dao/discount/SalesChannelMapper.java
@@ -0,0 +1,7 @@
+package com.yunya.middletable.dao.discount;
+
+import com.yunya.models.discount.SalesChannel;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SalesChannelMapper extends Mapper<SalesChannel> {
+}
