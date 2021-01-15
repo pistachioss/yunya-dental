9270caae7ff3b21af87b82b772887de17cb5edb2
lihuibin <951755155@qq.com>
2021-01-15 17:29:32 +0800

    修改:产品售出激活统计

--- yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/biz/DiscountBiz.java
+++ yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/biz/DiscountBiz.java
@@ -55,7 +55,7 @@ public class DiscountBiz {
 	 */
 	public PageInfo<CardStatisticsVo> getCardStatisticsPage(Integer couponId, CardStatisticsQuery query) {
 		Page<CardStatisticsVo> page = PageHelper.startPage(query.getPageNum(), query.getPageSize());
-		cardMapper.listCardByParam(query.getCardNumber(), query.getAllocateOrgIds(), query.getSoldTypes(),
+		List<CardStatisticsVo> cardStatisticsVos = cardMapper.listCardByParam(query.getCardNumber(), query.getAllocateOrgIds(), query.getSoldTypes(),
 				query.getSoldStartDate(), query.getSoldEndDate(), query.getActiveOrgIds(), query.getActiveStartDate(),
 				query.getActiveEndDate(), query.getSoldWays(), query.getChargeStatus(), couponId);
 		return new PageInfo<>(page);
