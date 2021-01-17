ff9e5a54d7454c7c5e218b75ea07ee4df52ecfe8
wy <wangye138386>
2021-01-17 13:25:10 +0800

    //患者服务

--- yunya-modules/yunya-patient-central/src/main/java/com/yunya/modules/patient_central/controller/app/AppPatientBaseInfoController.java
+++ yunya-modules/yunya-patient-central/src/main/java/com/yunya/modules/patient_central/controller/app/AppPatientBaseInfoController.java
@@ -12,11 +12,9 @@ import com.yunya.framework.common.utils.ResponseUtil;
 import com.yunya.modules.patient_central.biz.PatientBaseInfoBiz;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.Id;
 import java.util.List;
 
 /**
@@ -107,7 +105,6 @@ public class AppPatientBaseInfoController {
     @ApiOperation("员工推荐二维码")
     @GetMapping("staffQRCode/{id}")
     public ResponseResult staffQRCode(@PathVariable(value = "id") Integer id) {
-        /** 注入患者来源Mapper */
         String staffQRCode = this.patientBaseInfoBiz.staffQRCode(id);
         return ResponseUtil.success(staffQRCode);
     }
