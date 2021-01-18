b3304bb605c7f0ebb79298acd5ae06620ac5eca6
wy <wangye138386>
2021-01-18 16:29:02 +0800

    //患者服务

--- yunya-models/src/main/java/com/yunya/models/system/DictionaryItem.java
+++ yunya-models/src/main/java/com/yunya/models/system/DictionaryItem.java
@@ -1,11 +1,14 @@
 package com.yunya.models.system;
 
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.Date;
 
+@Data
 @Table(name = "dictionary_item")
 public class DictionaryItem {
     @Id
@@ -21,6 +24,11 @@ public class DictionaryItem {
      * 字典选项
      */
     private String name;
+
+    /**
+     * 英文字典选项
+     */
+    private String englishName;
 
     /**
      * 是否启用
@@ -60,175 +68,4 @@ public class DictionaryItem {
     @Column(name = "upd_time")
     private Date updTime;
 
-    /**
-     * @return id
-     */
-    public Integer getId() {
-        return id;
-    }
-
-    /**
-     * @param id
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * 获取字典类型ID
-     *
-     * @return dictionary_type_id - 字典类型ID
-     */
-    public Integer getDictionaryTypeId() {
-        return dictionaryTypeId;
-    }
-
-    /**
-     * 设置字典类型ID
-     *
-     * @param dictionaryTypeId 字典类型ID
-     */
-    public void setDictionaryTypeId(Integer dictionaryTypeId) {
-        this.dictionaryTypeId = dictionaryTypeId;
-    }
-
-    /**
-     * 获取字典选项
-     *
-     * @return name - 字典选项
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * 设置字典选项
-     *
-     * @param name 字典选项
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * 获取是否启用
-     *
-     * @return inservice - 是否启用
-     */
-    public Boolean getInservice() {
-        return inservice;
-    }
-
-    /**
-     * 设置是否启用
-     *
-     * @param inservice 是否启用
-     */
-    public void setInservice(Boolean inservice) {
-        this.inservice = inservice;
-    }
-
-    /**
-     * 获取创建人ID
-     *
-     * @return crt_id - 创建人ID
-     */
-    public Integer getCrtId() {
-        return crtId;
-    }
-
-    /**
-     * 设置创建人ID
-     *
-     * @param crtId 创建人ID
-     */
-    public void setCrtId(Integer crtId) {
-        this.crtId = crtId;
-    }
-
-    /**
-     * 获取创建人名称
-     *
-     * @return crt_name - 创建人名称
-     */
-    public String getCrtName() {
-        return crtName;
-    }
-
-    /**
-     * 设置创建人名称
-     *
-     * @param crtName 创建人名称
-     */
-    public void setCrtName(String crtName) {
-        this.crtName = crtName;
-    }
-
-    /**
-     * 获取创建时间
-     *
-     * @return crt_time - 创建时间
-     */
-    public Date getCrtTime() {
-        return crtTime;
-    }
-
-    /**
-     * 设置创建时间
-     *
-     * @param crtTime 创建时间
-     */
-    public void setCrtTime(Date crtTime) {
-        this.crtTime = crtTime;
-    }
-
-    /**
-     * @return upd_id
-     */
-    public Integer getUpdId() {
-        return updId;
-    }
-
-    /**
-     * @param updId
-     */
-    public void setUpdId(Integer updId) {
-        this.updId = updId;
-    }
-
-    /**
-     * 获取修改人名称
-     *
-     * @return upd_name - 修改人名称
-     */
-    public String getUpdName() {
-        return updName;
-    }
-
-    /**
-     * 设置修改人名称
-     *
-     * @param updName 修改人名称
-     */
-    public void setUpdName(String updName) {
-        this.updName = updName;
-    }
-
-    /**
-     * 获取修改时间
-     *
-     * @return upd_time - 修改时间
-     */
-    public Date getUpdTime() {
-        return updTime;
-    }
-
-    /**
-     * 设置修改时间
-     *
-     * @param updTime 修改时间
-     */
-    public void setUpdTime(Date updTime) {
-        this.updTime = updTime;
-    }
 }
