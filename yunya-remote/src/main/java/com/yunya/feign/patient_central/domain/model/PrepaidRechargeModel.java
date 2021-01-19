880507e3d0f04313ef6ac8dc1c6885077911fc02
wy <wangye138386>
2021-01-19 16:27:27 +0800

    //患者服务

--- yunya-remote/src/main/java/com/yunya/feign/patient_central/domain/model/PrepaidRechargeModel.java
+++ yunya-remote/src/main/java/com/yunya/feign/patient_central/domain/model/PrepaidRechargeModel.java
@@ -54,12 +54,6 @@ public class PrepaidRechargeModel implements Serializable {
     private PrepaidRechargeTollRecordModel prepaidRechargeTollRecordModel;
 
     /**
-     * 入账方式金额
-     */
-    @ApiModelProperty(value = "入账方式金额",required = true)
-    private BigDecimal paymentAmount;
-
-    /**
      * 充值卡卡号
      */
     @ApiModelProperty(value = "充值卡卡号")
