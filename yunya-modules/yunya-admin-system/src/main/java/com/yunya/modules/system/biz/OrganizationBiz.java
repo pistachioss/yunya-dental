5553d96037e234475ee62c42381f7cc7fdc4731d
zhou_wen <sa52305@outlook.com>
2021-01-30 13:51:42 +0800

    删除无效代码

--- yunya-modules/yunya-admin-system/src/main/java/com/yunya/modules/system/biz/OrganizationBiz.java
+++ yunya-modules/yunya-admin-system/src/main/java/com/yunya/modules/system/biz/OrganizationBiz.java
@@ -206,7 +206,6 @@ public class OrganizationBiz {
     company.setUpdTime(new Date(System.currentTimeMillis()));
     int i = companyMapper.updateByPrimaryKeySelective(company);
     redisUtils.delete(REDIS_KEY_ORG_LIST);
-    redisUtils.delete(RedisConstants.REDIS_KEY_ORG_ID + id);
     // 更新医疗机构扩展信息,并校验医疗机构简称是否重复
     updateOrganizationExtInfo(id, resource, companyType);
     // 发送消息
