410b5280dabd073725d1b2dfa7bdf80dcdae5354
lihuibin <951755155@qq.com>
2021-02-07 18:52:00 +0800

    修改:预约项目设置

--- yunya-modules/yunya-appointment-central/src/main/java/com/yunya/modules/appointment/biz/web/AppointItemBiz.java
+++ yunya-modules/yunya-appointment-central/src/main/java/com/yunya/modules/appointment/biz/web/AppointItemBiz.java
@@ -126,8 +126,13 @@ public class AppointItemBiz extends BaseBiz<AppointItemMapper, AppointItem> {
             if (StringHelper.isNotEmpty(clinicAppointItemByOrgId)) {
                 List<Integer> disableItemIds = clinicAppointItemByOrgId.stream().filter(entity -> entity.getInservice().equals(false)).map(ClinicAppointItem::getAppointItemId).collect(Collectors.toList());
                 if (StringHelper.isNotEmpty(disableItemIds)) {
-                    List<AppointmentItemVo> collect = ordersTypes.stream().filter(entity -> !disableItemIds.contains(entity.getId())).collect(Collectors.toList());
-                    return collect;
+//                    ordersTypes.forEach(entity->{
+//                        Integer id = entity.getId();
+//                        if (disableItemIds.contains(id)) {
+//                            entity.setInservice(false);
+//                        }
+//                    });
+                    return ordersTypes.stream().filter(entity->!disableItemIds.contains(entity.getId())).collect(Collectors.toList());
                 }
             }
         }
