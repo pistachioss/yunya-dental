e0d33565e16e725a577ccf88dee554420611a3da
chenlin <chenlinpsv@163.com>
2021-02-27 21:02:05 +0800

    修复bug

--- yunya-modules/yunya-sms/src/main/java/com/yunya/modules/sms/controller/SmsOrgStatisticsController.java
+++ yunya-modules/yunya-sms/src/main/java/com/yunya/modules/sms/controller/SmsOrgStatisticsController.java
@@ -2,12 +2,15 @@ package com.yunya.modules.sms.controller;
 
 import com.yunya.feign.sms.vo.SmsOrgStatisticsVO;
 import com.yunya.framework.common.annation.CurrentUser;
+import com.yunya.framework.common.constant.RedisConstants;
 import com.yunya.framework.common.context.BaseContextHandler;
 import com.yunya.framework.common.model.ResponseResult;
 import com.yunya.framework.common.utils.ResponseUtil;
+import com.yunya.framework.redis.util.RedisUtils;
 import com.yunya.modules.sms.biz.SmsOrgStatisticsBiz;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +31,8 @@ public class SmsOrgStatisticsController {
 
     @Autowired
     private SmsOrgStatisticsBiz smsOrgStatisticsBiz;
+    @Autowired
+    private RedisUtils redisUtils;
 
     /**
      * 根据orgId查询短信统计详情
@@ -41,5 +46,18 @@ public class SmsOrgStatisticsController {
         Integer orgId = Integer.parseInt(BaseContextHandler.getOrgId());
         SmsOrgStatisticsVO smsOrgStatisticsVO = smsOrgStatisticsBiz.findSmsOrgStatisticsByOrgId(orgId);
         return ResponseUtil.success(smsOrgStatisticsVO);
+    }
+
+    /**
+     * 清除缓存redis
+     *
+     * @return
+     */
+    @ApiOperation(value = "清除缓存redis")
+    @GetMapping("/clear")
+    public ResponseResult<T> clear(Integer orgId) {
+        redisUtils.delete(RedisConstants.SMS_STATISTICS_SURPLUS_ORG +orgId);
+        redisUtils.delete(RedisConstants.REDIS_KEY_ORG_LIST);
+        return ResponseUtil.success(null);
     }
 }
