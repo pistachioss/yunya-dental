7583491e6fd78582061a7a5d5599aa11911f6d38
me <1123038904@qq.com>
2021-03-05 15:22:49 +0800

    转诊管理

---
+++ yunya-remote/src/main/java/com/yunya/feign/treatment/domain/vo/ReferredInfoVO.java
@@ -0,0 +1,85 @@
+package com.yunya.feign.treatment.domain.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+
+import java.util.Date;
+
+/**
+ * 简介: 门诊端转诊记录VO
+ * <$>
+ *
+ * @author: 杨柳絮
+ * @date: $ $
+ * @description:
+ * @since: 1.0.0
+ * @param: $
+ * @return: $
+ */
+@Data
+@ApiModel(value = "ReferredInfoVO",description = "转诊记录视图模型")
+public class ReferredInfoVO {
+
+    private Integer id;
+
+    @ApiModelProperty("门诊id")
+    private Integer orgId;
+
+    @ApiModelProperty("患者id")
+    private Integer patientId;
+
+    @ApiModelProperty("患者名称")
+    private String patientName;
+
+    @ApiModelProperty("患者手机号")
+    private String telephone;
+
+    @ApiModelProperty("转诊人id")
+    private Integer userId;
+
+    @ApiModelProperty("转诊人名称")
+    private String userName;
+
+    @ApiModelProperty("转诊人的科室ID")
+    private Integer depId;
+
+    @ApiModelProperty("转诊人的科室名称")
+    private String depName;
+
+    @ApiModelProperty("转诊人挂号id")
+    private Integer registeredId;
+
+    @ApiModelProperty("被转诊人id")
+    private Integer referredId;
+
+    @ApiModelProperty("被转诊人名称")
+    private String referredName;
+
+    @ApiModelProperty("被转诊人的科室ID")
+    private Integer referredDepId;
+
+    @ApiModelProperty("被转诊人的科室名称")
+    private String referredDepName;
+
+    @ApiModelProperty("被转诊人挂号id")
+    private Integer referredRegisteredId;
+
+    @ApiModelProperty("备注")
+    private String remark;
+
+    @ApiModelProperty("创建人ID")
+    private Integer crtId;
+
+    @ApiModelProperty("创建时间")
+    private Date crtTime;
+
+    @ApiModelProperty("修改人ID")
+    private Integer updId;
+
+    @ApiModelProperty("修改时间")
+    private Date updTime;
+
+
+}
