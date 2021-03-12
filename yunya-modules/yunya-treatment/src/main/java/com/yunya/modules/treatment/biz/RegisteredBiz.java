21bbca4785f1de5c8a15082e0305ff69de8a5ce7
me <1123038904@qq.com>
2021-03-12 17:50:16 +0800

    报表

--- yunya-modules/yunya-treatment/src/main/java/com/yunya/modules/treatment/biz/RegisteredBiz.java
+++ yunya-modules/yunya-treatment/src/main/java/com/yunya/modules/treatment/biz/RegisteredBiz.java
@@ -211,8 +211,12 @@ public class RegisteredBiz extends BaseBiz<RegisteredMapper, Registered> {
                     vo -> {
                         vo.setPatientName(patMap.get(vo.getPatientId() + "").getName());
                         vo.setTelephone(patMap.get(vo.getPatientId() + "").getMobile());
-                        vo.setDepName(detMap.get(vo.getDepId() + "").getName());
-                        vo.setReferredDepName(detMap.get(vo.getReferredDepId() + "").getName());
+                        if (vo.getDepId() != null) {
+                            vo.setDepName(detMap.get(vo.getDepId() + "").getName());
+                        }
+                        if (vo.getReferredDepId() != null) {
+                            vo.setReferredDepName(detMap.get(vo.getReferredDepId() + "").getName());
+                        }
                         vo.setUserName(employeesMap.get(vo.getUserId() + "").getName());
                         vo.setReferredName(employeesMap.get(vo.getReferredId() + "").getName());
                     });
