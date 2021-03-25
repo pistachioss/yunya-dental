b1c37570be461a1e2af63e1aca27628f539f2a9a
chenlin <chenlinpsv@163.com>
2021-03-25 15:46:50 +0800

    修复bug

--- yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/controller/CompanyReportOfFinanceController.java
+++ yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/controller/CompanyReportOfFinanceController.java
@@ -831,13 +831,13 @@ public class CompanyReportOfFinanceController {
   }
 
   /**
-   * 根据条件查询折扣&免单列表
+   * 根据条件查询折扣&免单报表导出
    *
    * @param query 查询条件
    * @return
    */
-  @ApiOperation("公司端报表-财务报表-折扣&免单报表")
-  @PostMapping(value = "/bill/discountAndFreePayment/export", name = "根据条件查询折扣&免单列表")
+  @ApiOperation("公司端报表-财务报表-折扣&免单报表导出")
+  @PostMapping(value = "/bill/discountAndFreePayment/export", name = "根据条件查询折扣&免单报表导出")
   public ResponseResult<T> billDiscountAndFreePaymentExport(HttpServletResponse response,
           @RequestBody @Validated BillDiscountAndFreePaymentQuery query) throws IOException {
     billPayBiz.billDiscountAndFreePaymentExport(query, response);
