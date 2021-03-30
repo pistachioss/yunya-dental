25e767eb5cb7b364739824f31fd024ee44999b26
lihuibin <951755155@qq.com>
2021-03-30 10:13:55 +0800

    修改:预约可视图请假排班信息

--- yunya-remote/src/main/java/com/yunya/feign/appointment/vo/EmpScheduleVo.java
+++ yunya-remote/src/main/java/com/yunya/feign/appointment/vo/EmpScheduleVo.java
@@ -25,7 +25,7 @@ public class EmpScheduleVo implements Serializable {
     private Integer companyId;
     @ApiModelProperty("门诊名称")
     private String comName;
-    @ApiModelProperty("排班（上班/休息）; 假期类型(班次/天)")
+    @ApiModelProperty("排班（上班/休息）; 假期类型(班次/天)；外勤；加班")
     private String status;
     @ApiModelProperty("排班/请假 开始日期")
     private String startDate;
