14dc9f2a1ac609bab9945ab2aa036997c9a7f737
me <1123038904@qq.com>
2021-03-30 10:10:47 +0800

    请假

--- yunya-remote/src/main/java/com/yunya/feign/employee_attend/vo/LeaveInfoListVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/employee_attend/vo/LeaveInfoListVO.java
@@ -110,4 +110,11 @@ public class LeaveInfoListVO {
     @ApiModelProperty("请假时长(分钟)")
     private Integer leaveTime;
 
+    @ApiModelProperty("班次开始时间（后端使用）")
+    @JsonFormat(pattern = "HH:mm:ss",timezone = "GMT+8")
+    private Date LsStartDate;
+    @ApiModelProperty("班次结束时间（后端使用）")
+    @JsonFormat(pattern = "HH:mm:ss",timezone = "GMT+8")
+    private Date LsEndDate;
+
 }
