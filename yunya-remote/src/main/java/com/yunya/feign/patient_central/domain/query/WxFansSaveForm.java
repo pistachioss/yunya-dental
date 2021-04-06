b224df1b06de7e801e3089f64411f700943c0513
me <1123038904@qq.com>
2021-04-06 14:58:54 +0800

    客服中心

---
+++ yunya-remote/src/main/java/com/yunya/feign/patient_central/domain/query/WxFansSaveForm.java
@@ -0,0 +1,27 @@
+package com.yunya.feign.patient_central.domain.query;
+
+import com.yunya.models.patient_central.WxFans;
+import com.yunya.models.patient_central.WxFansBind;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * 简介:
+ * <$>
+ *
+ * @author: 杨柳絮
+ * @date: $ $
+ * @description:
+ * @since: 1.0.0
+ * @param: $
+ * @return: $
+ */
+@Data
+public class WxFansSaveForm {
+
+    @ApiModelProperty("公司微信公众号粉丝信息")
+    private WxFans wxFans;
+    @ApiModelProperty("绑定关系")
+    private WxFansBind fansBind;
+
+}
