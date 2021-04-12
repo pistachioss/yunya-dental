44e9f8d088fbbcf59fab8a8e52ecdf7eccf76b45
zhou_wen <sa52305@outlook.com>
2021-04-12 15:18:50 +0800

    增加收费项目及工作量列表导出

--- yunya-remote/src/main/java/com/yunya/feign/report/domain/vo/BillItemTollAndWorkloadVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/report/domain/vo/BillItemTollAndWorkloadVO.java
@@ -1,5 +1,6 @@
 package com.yunya.feign.report.domain.vo;
 
+import com.yunya.framework.common.annation.Excel;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -23,34 +24,42 @@ public class BillItemTollAndWorkloadVO implements Serializable {
   /** 项目分类ID */
   @ApiModelProperty("项目分类ID")
   private Integer itemCategoryId;
-  /** 项目分类名称 */
-  @ApiModelProperty("项目分类名称")
-  private String itemCategoryName;
   /** 项目明细ID */
   @ApiModelProperty("项目明细ID")
   private Integer itemId;
   /** 项目明细编号 */
+  @Excel(name = "项目编号")
   @ApiModelProperty("项目明细编号")
   private String itemNum;
-  /** 项目明细名称 */
-  @ApiModelProperty("项目明细名称")
-  private String itemName;
   /** 执行人ID */
   @ApiModelProperty("执行人ID")
   private Integer executorId;
   /** 执行人姓名 */
+  @Excel(name = "执行人")
   @ApiModelProperty("执行人姓名")
   private String executorName;
+  /** 项目分类名称 */
+  @Excel(name = "所属分类")
+  @ApiModelProperty("项目分类名称")
+  private String itemCategoryName;
+  /** 项目明细名称 */
+  @Excel(name = "项目名称")
+  @ApiModelProperty("项目明细名称")
+  private String itemName;
   /** 已收工作量 */
+  @Excel(name = "已收工作量")
   @ApiModelProperty("已收工作量")
   private BigDecimal receivedWorkload;
   /** 免单工作量 */
+  @Excel(name = "其中免单工作量")
   @ApiModelProperty("免单工作量")
   private BigDecimal freePayWorkload;
   /** 补入工作量 */
+  @Excel(name = "补入工作量")
   @ApiModelProperty("补入工作量")
   private BigDecimal supplyWorkload;
   /** 退费工作量 */
+  @Excel(name = "退费工作量")
   @ApiModelProperty("退费工作量")
   private BigDecimal refundWorkload;
 }
