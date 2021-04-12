ab8ddbb852ac2cc94a26d42cc0d054800273b36f
zhou_wen <sa52305@outlook.com>
2021-04-12 11:42:41 +0800

    更新运营数据总览补入工作量查询

--- yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/mapper/BaseBillDetailMapper.java
+++ yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/mapper/BaseBillDetailMapper.java
@@ -149,6 +149,14 @@ public interface BaseBillDetailMapper extends Mapper<BaseBillDetail> {
   WorkloadStatisticsVO selectClinicWorkloadStatistic(@Param("query") DataStatisticsQuery query);
 
   /**
+   * 根据条件查询门诊补入工作量列表
+   *
+   * @param query 查询条件
+   * @return list
+   */
+  List<BillRecordWorkloadVO> selectCouponWorkloadList(@Param("query") DataStatisticsQuery query);
+
+  /**
    * 根据条件查询门诊当月账单明细列表
    *
    * @param query 查询条件
