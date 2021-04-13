a4ce8c2ceea1868681979238e89695f89354705f
zhou_wen <sa52305@outlook.com>
2021-04-13 17:24:08 +0800

    更新工作量金额字段精度为2位小数

--- yunya-remote/src/main/java/com/yunya/feign/report/domain/vo/EmployeeWorkloadOfOperationVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/report/domain/vo/EmployeeWorkloadOfOperationVO.java
@@ -38,39 +38,39 @@ public class EmployeeWorkloadOfOperationVO implements Serializable {
   @ApiModelProperty("在职状态（0-试用；1-转正；2-离职）")
   private Byte workStatus;
   /** 实收工作量（元） */
-  @Excel(name = "实收工作量（元）", cellType = Excel.ColumnType.NUMERIC)
+  @Excel(name = "实收工作量（元）", scale = 2, cellType = Excel.ColumnType.NUMERIC)
   @ApiModelProperty("实收工作量（元）")
   private BigDecimal actualWorkload;
   /** 已收工作量（元） */
-  @Excel(name = "已收工作量（元）", cellType = Excel.ColumnType.NUMERIC)
+  @Excel(name = "已收工作量（元）", scale = 2, cellType = Excel.ColumnType.NUMERIC)
   @ApiModelProperty("已收工作量（元）")
   private BigDecimal receivedWorkload;
   /** 免单支付工作量（元） */
-  @Excel(name = "免单支付工作量（元）", cellType = Excel.ColumnType.NUMERIC)
+  @Excel(name = "免单支付工作量（元）", scale = 2, cellType = Excel.ColumnType.NUMERIC)
   @ApiModelProperty("免单支付工作量（元）")
   private BigDecimal freePaymentWorkload;
   /** 补入工作量（元） */
-  @Excel(name = "补入工作量（元）", cellType = Excel.ColumnType.NUMERIC)
+  @Excel(name = "补入工作量（元）", scale = 2, cellType = Excel.ColumnType.NUMERIC)
   @ApiModelProperty("补入工作量（元）")
   private BigDecimal supplementWorkload;
   /** 退费工作量（元） */
-  @Excel(name = "退费工作量（元）", cellType = Excel.ColumnType.NUMERIC)
+  @Excel(name = "退费工作量（元）", cellType = Excel.ColumnType.NUMERIC, scale = 2)
   @ApiModelProperty("退费工作量（元）")
   private BigDecimal refundWorkload;
   /** 加工费（元） */
-  @Excel(name = "加工费（元）", cellType = Excel.ColumnType.NUMERIC)
+  @Excel(name = "加工费（元）", cellType = Excel.ColumnType.NUMERIC, scale = 2)
   @ApiModelProperty("加工费（元）")
   private BigDecimal processingFee;
   /** 正畸加工费（元） */
-  @Excel(name = "正畸加工费（元）", cellType = Excel.ColumnType.NUMERIC)
+  @Excel(name = "正畸加工费（元）", cellType = Excel.ColumnType.NUMERIC, scale = 2)
   @ApiModelProperty("正畸加工费（元）")
   private BigDecimal orthodonticsFee;
   /** 基本工作量（元） */
-  @Excel(name = "基本工作量（元）", cellType = Excel.ColumnType.NUMERIC)
+  @Excel(name = "基本工作量（元）", cellType = Excel.ColumnType.NUMERIC, scale = 2)
   @ApiModelProperty("基本工作量（元）")
   private BigDecimal baseWorkload;
   /** 大额材料费（元） */
-  @Excel(name = "大额材料费（元）", cellType = Excel.ColumnType.NUMERIC)
+  @Excel(name = "大额材料费（元）", cellType = Excel.ColumnType.NUMERIC, scale = 2)
   @ApiModelProperty("大额材料费（元）")
   private BigDecimal largeMaterialCost;
 }
