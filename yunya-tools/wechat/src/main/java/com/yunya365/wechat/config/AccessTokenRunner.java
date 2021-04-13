e26440ff6a8b2354e96c2ae6fa70f8529b90f69a
xiangyang <xiangyang@qq.com>
2021-04-13 14:20:17 +0800

    feat(wechat): 微信auth auth

--- yunya-tools/wechat/src/main/java/com/yunya365/wechat/config/AccessTokenRunner.java
+++ yunya-tools/wechat/src/main/java/com/yunya365/wechat/config/AccessTokenRunner.java
@@ -1,15 +1,14 @@
 package com.yunya365.wechat.config;
 
-import com.yunya.feign.wechat.domain.vo.WxAccessTokenVo;
-import com.yunya.framework.common.constant.WXConstant;
-import com.yunya.framework.redis.util.RedisUtils;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import com.yunya.feign.wechat.domain.vo.*;
+import com.yunya.framework.common.constant.*;
+import com.yunya.framework.redis.util.*;
+import lombok.extern.slf4j.*;
+import org.apache.commons.lang3.*;
+import org.springframework.web.client.*;
 
-import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
+import javax.annotation.*;
+import java.util.concurrent.*;
 
 /**
  * @description:
@@ -28,21 +27,16 @@ public class AccessTokenRunner{
 
     public void refreshToken(){
         String accessToken = null;
-        String redisKey = String.format(WXConstant.ACCESS_TOKEN_KEY, wxConfig.getAppId());
-        accessToken = redisUtils.get(redisKey);
-        if (StringUtils.isNotBlank(accessToken)) {
-            log.info("access_token已存在，不需要刷新：{}", accessToken);
-            return;
-        }
         String url = String.format(WXConstant.WX_ACCESS_TOKEN_URL, wxConfig.getAppId(), wxConfig.getAppSecret());
         log.info("获取access_token的url：{}", url);
-        //HttpClient工具根据项目自行修改
         WxAccessTokenVo accessTokenRes = restTemplate.getForObject(url, WxAccessTokenVo.class);
         log.info("调用微信access_token返回结果是: {}", accessTokenRes);
         if (accessTokenRes == null || StringUtils.isNotBlank(accessTokenRes.getErrcode())) {
+            log.info("公众号token获取失败");
             return;
         }
         accessToken = accessTokenRes.getAccess_token();
+        String redisKey = String.format(WXConstant.ACCESS_TOKEN_KEY, wxConfig.getAppId());
         //redis工具根据项目自行修改
         redisUtils.set(redisKey, accessToken, 7200, TimeUnit.SECONDS);
     }
