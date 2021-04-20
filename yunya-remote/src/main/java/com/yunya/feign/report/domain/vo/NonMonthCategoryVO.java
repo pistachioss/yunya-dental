dbe5ca46b75762cd7fab7f61f99e9d49eb24a692
chenlin <chenlinpsv@163.com>
2021-04-20 09:40:22 +0800

    项目分类收入汇总，数据列排列优化，优惠金额与实收金额位置调换

--- yunya-remote/src/main/java/com/yunya/feign/report/domain/vo/NonMonthCategoryVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/report/domain/vo/NonMonthCategoryVO.java
@@ -57,12 +57,12 @@ public class NonMonthCategoryVO implements Serializable {
     @Excel(name = "免单时间")
     @ApiModelProperty("免单时间")
     private String freeDate;
-    /** 非当月补入工作量*/
-    @Excel(name = "非当月补入工作量", scale = 2, cellType = Excel.ColumnType.NUMERIC)
-    @ApiModelProperty("非当月补入工作量")
-    private BigDecimal couponWorkload = BigDecimal.ZERO;
     /** 非当月免单金额分摊*/
     @Excel(name = "非当月免单金额分摊", scale = 2, cellType = Excel.ColumnType.NUMERIC)
     @ApiModelProperty("非当月免单金额分摊")
     private BigDecimal freeAmount = BigDecimal.ZERO;
+    /** 非本月账单项目补入工作量*/
+    @Excel(name = "非本月账单项目补入工作量", scale = 2, cellType = Excel.ColumnType.NUMERIC)
+    @ApiModelProperty("非本月账单项目补入工作量")
+    private BigDecimal couponWorkload = BigDecimal.ZERO;
 }
