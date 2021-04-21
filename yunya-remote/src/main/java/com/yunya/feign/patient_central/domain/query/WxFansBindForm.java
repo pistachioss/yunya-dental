19105440e4bd66ef33db19c5e7218f3c2dbfada8
me <1123038904@qq.com>
2021-04-21 09:24:33 +0800

    bug

--- yunya-remote/src/main/java/com/yunya/feign/patient_central/domain/query/WxFansBindForm.java
+++ yunya-remote/src/main/java/com/yunya/feign/patient_central/domain/query/WxFansBindForm.java
@@ -1,5 +1,6 @@
 package com.yunya.feign.patient_central.domain.query;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,13 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @return: $
  */
 @Data
+@ApiModel
 public class WxFansBindForm {
 
     @ApiModelProperty("公司微信公众号粉丝表Id")
     private Integer wxId;
 
-    @ApiModelProperty("患者ID")
+    @ApiModelProperty("解绑患者ID")
     private Integer patientId;
 
     @ApiModelProperty("openID")
