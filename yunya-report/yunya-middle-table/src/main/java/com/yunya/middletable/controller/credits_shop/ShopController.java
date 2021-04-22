4809f1cdc180e2844f38040aeebf39c98d677be6
lihuibin <951755155@qq.com>
2021-04-22 18:01:37 +0800

    修改参数封装

--- yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/controller/credits_shop/ShopController.java
+++ yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/controller/credits_shop/ShopController.java
@@ -2,10 +2,15 @@ package com.yunya.middletable.controller.credits_shop;
 
 import com.yunya.middletable.service.credits_shop.CreditsShopBiz;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @program: yunya-dental
@@ -16,9 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @Api(tags = "积分商城")
-@RequestMapping("/credits/shop")
+@RequestMapping("/duiba/credits/shop")
 public class ShopController {
     @Autowired
     private CreditsShopBiz creditsShopBiz;
 
+    @ApiOperation("生成免登录URL")
+    @GetMapping("/dautoLogin")
+    public String duibaAutoLogin(HttpServletRequest request) {
+
+        // TODO 生成免登录URL
+        return "";
+    }
 }
