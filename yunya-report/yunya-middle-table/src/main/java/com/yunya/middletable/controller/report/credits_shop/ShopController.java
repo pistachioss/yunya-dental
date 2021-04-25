e22cc41d709365c2e6dc22d66edd4c3bc5474918
lihuibin <951755155@qq.com>
2021-04-25 13:13:01 +0800

    引用包

--- yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/controller/report/credits_shop/ShopController.java
+++ yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/controller/report/credits_shop/ShopController.java
@@ -11,11 +11,7 @@ import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
