960ffbcf66f8d5118461d888384df9a9da4135d3
zhou_wen <sa52305@outlook.com>
2021-05-07 10:38:33 +0800

    新增定时任务支持注解

--- yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/YunyaReportUltimateApplication.java
+++ yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/YunyaReportUltimateApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -22,6 +23,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.yunya.report.ultimate.mapper")
 @EnableTransactionManagement
 @EnableDiscoveryClient
+@EnableScheduling
 @ComponentScan(basePackages = {"com.yunya.report.ultimate", "com.yunya.framework"})
 @EnableCustomSwagger2
 public class YunyaReportUltimateApplication {
