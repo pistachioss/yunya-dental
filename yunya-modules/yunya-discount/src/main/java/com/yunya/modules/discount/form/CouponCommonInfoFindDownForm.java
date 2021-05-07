c224aadcc87ba5f31de8c75e370dd52634bb5f32
me <1123038904@qq.com>
2021-05-07 09:30:30 +0800

    bug

---
+++ yunya-modules/yunya-discount/src/main/java/com/yunya/modules/discount/form/CouponCommonInfoFindDownForm.java
@@ -0,0 +1,21 @@
+package com.yunya.modules.discount.form;
+
+import lombok.Data;
+
+/**
+ * 简介:
+ * <$>
+ *
+ * @author: 杨柳絮
+ * @date: $ $
+ * @description:
+ * @since: 1.0.0
+ * @param: $
+ * @return: $
+ */
+@Data
+public class CouponCommonInfoFindDownForm {
+
+    private String name;
+
+}
