bd385467ad85cf1f589af83f64b02eff5ffcea9b
chenlin <chenlinpsv@163.com>
2021-05-19 13:16:49 +0800

    公司端-运营分析-数据总览导出优化

--- yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/utils/poi/RowStyle.java
+++ yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/utils/poi/RowStyle.java
@@ -2,7 +2,7 @@ package com.yunya.framework.common.utils.poi;
 
 public class RowStyle {
     /** 该行单元格宽度*/
-    public static final String CELL_WiTH = "cellWith";
+    public static final String CELL_WIDTH = "cellWith";
     /** 该行高度*/
     public static final String ROW_HEIGHT = "rowHeight";
     /** 该行是否加粗：true/false */
