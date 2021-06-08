e9f956d32b4e60dbaabdc50340c6ef28bb1f7857
lihuibin <951755155@qq.com>
2021-06-08 10:12:25 +0800

    修改预约列表时间查询接口描述

--- yunya-modules/yunya-appointment-central/src/main/java/com/yunya/modules/appointment/controller/app/OnlineAppointmentController.java
+++ yunya-modules/yunya-appointment-central/src/main/java/com/yunya/modules/appointment/controller/app/OnlineAppointmentController.java
@@ -123,10 +123,10 @@ public class OnlineAppointmentController {
      */
     @ApiOperation("查询预约时间列表")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "门诊ID",value = "orgId",required = true,dataTypeClass = Integer.class),
-            @ApiImplicitParam(name = "项目ID",value = "itemId",required = true,dataTypeClass = Integer.class),
-            @ApiImplicitParam(name = "预约日期",value = "date",required = true,dataTypeClass = String.class,defaultValue = "2021-06-01"),
-            @ApiImplicitParam(name = "预约时间，如果传入时间，会将该时间渲染到时间列表指定时间段",value = "time",required = false,dataTypeClass = String.class,defaultValue = "")
+            @ApiImplicitParam(name = "orgId",value = "门诊ID",required = true,dataTypeClass = Integer.class),
+            @ApiImplicitParam(name = "itemId",value = "项目ID",required = true,dataTypeClass = Integer.class),
+            @ApiImplicitParam(name = "date",value = "预约日期",required = true,dataTypeClass = String.class,defaultValue = "2021-06-01"),
+            @ApiImplicitParam(name = "time",value = "预约时间，如果传入时间，会将该时间渲染到时间列表指定时间段",required = false,dataTypeClass = String.class,defaultValue = "")
     })
     @GetMapping("/time/list/{orgId}/{itemId}")
     public ResponseResult<Map<String,List<CountOnlineAppointVo>>>  appointTimeList(@PathVariable("orgId") @NotNull(message = "门诊ID不能为空") Integer orgId,
