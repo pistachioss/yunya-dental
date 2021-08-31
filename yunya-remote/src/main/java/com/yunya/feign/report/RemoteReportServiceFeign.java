059e54d8db392cadb69db447b79421fc8d23e635
wy <wangye138386>
2021-08-31 14:14:57 +0800

    [新增功能](dev):
    搜索患者添加雅雯门诊搜索需求

--- yunya-remote/src/main/java/com/yunya/feign/report/RemoteReportServiceFeign.java
+++ yunya-remote/src/main/java/com/yunya/feign/report/RemoteReportServiceFeign.java
@@ -1,6 +1,8 @@
 package com.yunya.feign.report;
 
 import com.github.pagehelper.PageInfo;
+import com.yunya.feign.patient_central.domain.query.PatientLikeFinleQueryForm;
+import com.yunya.feign.patient_central.domain.vo.web.PatientBaseInfoVo;
 import com.yunya.feign.report.domain.query.TreatmentList4AppQuery;
 import com.yunya.feign.report.domain.vo.BaseTreatmentProcessVO;
 import com.yunya.feign.report.domain.vo.BenefitItemVo;
@@ -9,12 +11,7 @@ import com.yunya.feign.wechat.domain.model.WxTemplateMsgModel;
 import com.yunya.feign.wechat.domain.vo.WxAppointConfirmPushVo;
 import com.yunya.framework.common.constant.YunyaServiceNameConstants;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,4 +35,7 @@ public interface RemoteReportServiceFeign {
 
     @GetMapping("/api/appoint/confirm/push/list")
     List<WxTemplateMsgModel> listPushConfirmAppoint();
+
+    @RequestMapping(value = "/api/patient/likePatient",method = RequestMethod.POST)
+    List<PatientBaseInfoVo> findPatientLikePatientInfo(@RequestBody PatientLikeFinleQueryForm form);
 }
