950b3a63714128b152aa90e07f4068b6147a543f
Yunya_Zhouwen <sa52305@outlook.com>
2021-09-23 13:33:40 +0800

    [新增功能](dev): 更新产品激活明细
    1、产品激活明细增加患者初诊日期字段；

--- yunya-remote/src/main/java/com/yunya/feign/report/domain/vo/CardActiveVo.java
+++ yunya-remote/src/main/java/com/yunya/feign/report/domain/vo/CardActiveVo.java
@@ -15,6 +15,9 @@ public class CardActiveVo {
 	@ApiModelProperty(value = "患者")
 	@ExcelProperty("患者")
 	private String patientName;
+	@ExcelProperty("初诊日期")
+	@ApiModelProperty("初诊日期")
+	private String firstTreatDate;
 	@ApiModelProperty(value = "激活时间")
 	@ExcelProperty("激活时间")
 	private String activeDate;
