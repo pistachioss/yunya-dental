665a51e8c29adaaa911b2f7e587232226d863e5c
Yunya_Zhouwen <sa52305@outlook.com>
2021-10-12 12:57:33 +0800

    [新增功能](dev): 更新门诊价目表修改

--- yunya-modules/yunya-treatment/src/test/java/com/yunya/modules/treatment/controller/ClinicTariffControllerTest.java
+++ yunya-modules/yunya-treatment/src/test/java/com/yunya/modules/treatment/controller/ClinicTariffControllerTest.java
@@ -1,5 +1,7 @@
 package com.yunya.modules.treatment.controller;
 
+import com.yunya.feign.treatment.domain.form.ClinicItemMemberPriceForm;
+import com.yunya.feign.treatment.domain.form.ClinicTariffForm;
 import com.yunya.feign.treatment.domain.vo.ClinicTariffVO;
 import com.yunya.framework.common.model.ResponseResult;
 import com.yunya.modules.treatment.controller.web.ClinicTariffController;
@@ -9,6 +11,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 简介: 门诊价目表控制层测试
@@ -32,6 +38,30 @@ public class ClinicTariffControllerTest {
   @Test
   public void testSwitchTariff() {
     ResponseResult<T> result = clinicTariffController.switchClinicTariff(35, 201090);
+    System.out.println(result);
+  }
+
+  @Test
+  public void modifyClinicTariff() {
+    ClinicTariffForm form = new ClinicTariffForm();
+    form.setOrgId(26);
+    form.setTariffId(1);
+    form.setPrice(BigDecimal.valueOf(500));
+    List<ClinicItemMemberPriceForm> list = new ArrayList<>();
+    ClinicItemMemberPriceForm memberPriceForm = new ClinicItemMemberPriceForm();
+    memberPriceForm.setMemberTypeId(1);
+    memberPriceForm.setDiscountPrice(BigDecimal.valueOf(401));
+    list.add(memberPriceForm);
+    ClinicItemMemberPriceForm memberPriceForm1 = new ClinicItemMemberPriceForm();
+    memberPriceForm1.setMemberTypeId(2);
+    memberPriceForm1.setDiscountPrice(BigDecimal.valueOf(301));
+    list.add(memberPriceForm1);
+    ClinicItemMemberPriceForm memberPriceForm2 = new ClinicItemMemberPriceForm();
+    memberPriceForm2.setMemberTypeId(3);
+    memberPriceForm2.setDiscountPrice(BigDecimal.valueOf(201));
+    list.add(memberPriceForm2);
+    form.setClinicItemMemberPrices(list);
+    ResponseResult<T> result = clinicTariffController.modify(form);
     System.out.println(result);
   }
 }
