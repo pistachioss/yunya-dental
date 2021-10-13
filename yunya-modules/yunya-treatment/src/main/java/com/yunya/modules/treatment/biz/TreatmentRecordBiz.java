0d4ac094a1dc85df3fbc9d9f189c5b6617d9625a
chenlin <chenlinpsv@163.com>
2021-10-13 15:31:21 +0800

    [代码完善](dev_20210831_clinicTreatStatsticsRemind): 修复bug
    查询就诊统计时，增加参数orgId

--- yunya-modules/yunya-treatment/src/main/java/com/yunya/modules/treatment/biz/TreatmentRecordBiz.java
+++ yunya-modules/yunya-treatment/src/main/java/com/yunya/modules/treatment/biz/TreatmentRecordBiz.java
@@ -1757,8 +1757,8 @@ public class TreatmentRecordBiz extends BaseBiz<TreatmentRecordMapper, Treatment
     model.setOrgIds(Arrays.asList(orgId));
     List<SysUserInfoDetail> employee = systemServiceFeign.findSysUserEmployeeInfoList(model);
     if (StringHelper.isEmpty(employee)) {
-      //无权限
-      return ResponseUtil.fail(OPERATION_NOT_ALLOW,"用户无权限", null);
+      // 用户无权限
+      return ResponseUtil.success(null);
     }
     String currentDate = DateTime.now().toString();
     TreatmentCountQuery query = new TreatmentCountQuery();
@@ -1769,7 +1769,8 @@ public class TreatmentRecordBiz extends BaseBiz<TreatmentRecordMapper, Treatment
             + resultData.getTreatCompleted()
             + resultData.getTreatReceiving();
     if (unCheckedOut <= 0) {
-      return ResponseUtil.fail(DATA_NOT_EXIST,"门诊无数据",null);
+      // 无数据
+      return ResponseUtil.success(null);
     }
     resultData.setUnCheckedOut(unCheckedOut);
     return ResponseUtil.success(resultData);
