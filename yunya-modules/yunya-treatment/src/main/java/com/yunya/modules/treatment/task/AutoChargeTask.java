fe19c466e22173adf5c8938ab3c33a3551b3abeb
Yunya_Zhouwen <sa52305@outlook.com>
2021-10-21 14:18:13 +0800

    [新增功能](dev): 添加定时任务自动收费

--- yunya-modules/yunya-treatment/src/main/java/com/yunya/modules/treatment/task/AutoChargeTask.java
+++ yunya-modules/yunya-treatment/src/main/java/com/yunya/modules/treatment/task/AutoChargeTask.java
@@ -1,10 +1,12 @@
 package com.yunya.modules.treatment.task;
 
-import com.yunya.feign.treatment.domain.model.InvoiceModel;
-import com.yunya.feign.treatment.domain.model.TollModel;
+import com.yunya.feign.rabbitmq.RemoteRabbitMqServiceFeign;
 import com.yunya.framework.common.constant.BusinessConstants;
 import com.yunya.framework.common.utils.DateUtil;
+import com.yunya.models.treatment.BillRecord;
 import com.yunya.models.treatment.OrderRecord;
+import com.yunya.models.treatment.TreatmentRecord;
+import com.yunya.modules.treatment.biz.BillRecordBiz;
 import com.yunya.modules.treatment.biz.OrderRecordBiz;
 import com.yunya.modules.treatment.biz.TollBiz;
 import com.yunya.modules.treatment.biz.TreatmentRecordBiz;
@@ -12,12 +14,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
+
+import static com.yunya.feign.report.enums.MsgCategoryEnum.BaseBill;
+import static com.yunya.feign.report.enums.MsgCategoryEnum.BaseTreatmentProcess;
 
 /**
  * 简介: 自动收费定时任务
@@ -40,8 +47,17 @@ public class AutoChargeTask {
   @Resource private OrderRecordBiz orderRecordBiz;
   /** 收费业务层 */
   @Resource private TollBiz tollBiz;
+  /** 账单业务层 */
+  @Resource private BillRecordBiz billRecordBiz;
+  /** 消息队列 */
+  @Resource private RemoteRabbitMqServiceFeign rabbitMqServiceFeign;
 
-  @Scheduled(cron = "0 15 09 * * ?")
+  /**
+   * 定时任务自动收费
+   *
+   * @throws InterruptedException
+   */
+  @Scheduled(cron = "0 30 14 * * ?")
   public void autoCharge() throws InterruptedException {
     // 获取当天全部就诊完成数据列表
     List<Integer> treatmentRecordIds =
@@ -57,15 +73,32 @@ public class AutoChargeTask {
                 executorService.submit(
                     () -> {
                       try {
-                        TollModel model = new TollModel();
-                        model.setOrderRecordId(orderRecord.getId());
-                        model.setDiscountType((byte) 0);
-                        model.setOutstandingAmount(orderRecord.getTotalAmount());
-                        InvoiceModel invoiceModel = new InvoiceModel();
-                        invoiceModel.setInvoice(false);
-                        model.setInvoiceModel(invoiceModel);
-                        model.setIsAutoChecked(true);
-                        tollBiz.confirmCharge(model);
+                        // 更新就诊记录状态为离店
+                        TreatmentRecord treatmentRecord =
+                            treatmentRecordBiz.selectById(orderRecord.getTreatmentRecordId());
+                        treatmentRecord.setStatus(
+                            BusinessConstants.TREATMENT_PROCESS_FINISH_STATUS);
+                        treatmentRecordBiz.updateTreatmentStatus(treatmentRecord);
+                        Integer appointmentId = treatmentRecord.getAppointmentId();
+                        if (!ObjectUtils.isEmpty(appointmentId)) {
+                          rabbitMqServiceFeign.sendMessage(
+                              appointmentId, 0, 1, BaseTreatmentProcess);
+                        } else {
+                          rabbitMqServiceFeign.sendMessage(
+                              treatmentRecord.getRegisteredId(), 1, 1, BaseTreatmentProcess);
+                        }
+                        // 保存账单记录
+                        BillRecord billRecord = buildBillRecord(orderRecord);
+                        billRecordBiz.insertSelective(billRecord);
+                        // 保存开单明细收费记录
+                        tollBiz.saveOrderDetailPayRecordWithNoDiscount(
+                            BigDecimal.ZERO, orderRecord.getId(), billRecord.getId(), true);
+                        // 更新开单状态为结账
+                        orderRecord.setStatus(BusinessConstants.ORDER_FINISH_STATUS);
+                        int updateOrderStatus = orderRecordBiz.updateOrderStatus(orderRecord);
+                        if (updateOrderStatus > 0) {
+                          rabbitMqServiceFeign.sendMessage(orderRecord.getId(), 1, BaseBill);
+                        }
                       } finally {
                         countDownLatch.countDown();
                       }
@@ -73,5 +106,30 @@ public class AutoChargeTask {
         countDownLatch.await();
       }
     }
+  }
+
+  /**
+   * 构建账单记录
+   *
+   * @param orderRecord
+   * @return
+   */
+  private BillRecord buildBillRecord(OrderRecord orderRecord) {
+    BillRecord billRecord =
+        tollBiz.generateBillRecord(
+            orderRecord.getTreatmentRecordId(),
+            orderRecord.getPatientId(),
+            orderRecord.getId(),
+            orderRecord.getOrgId());
+    BigDecimal totalAmount = orderRecord.getTotalAmount();
+    billRecord.setReceivableAmount(totalAmount);
+    billRecord.setPrivilegeAmount(BigDecimal.ZERO);
+    billRecord.setActualReceivableAmount(totalAmount);
+    billRecord.setReceivedAmount(BigDecimal.ZERO);
+    billRecord.setDebtAmount(totalAmount);
+    billRecord.setInvoice(false);
+    billRecord.setCrtId(BusinessConstants.ADMIN_ID);
+    billRecord.setCrtName(BusinessConstants.ADMIN_NAME);
+    return billRecord;
   }
 }
