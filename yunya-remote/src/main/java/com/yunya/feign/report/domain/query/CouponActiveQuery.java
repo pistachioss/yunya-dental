58ea4350d4ac8f6c4e3dab3a6f19e36276f912c4
me <1123038904@qq.com>
2021-12-27 13:15:01 +0800

    激活门诊增加筛选

--- yunya-remote/src/main/java/com/yunya/feign/report/domain/query/CouponActiveQuery.java
+++ yunya-remote/src/main/java/com/yunya/feign/report/domain/query/CouponActiveQuery.java
@@ -13,7 +13,7 @@ import java.util.List;
  * @author: xy
  * @date 2021/3/17 14:27
  **/
-@ApiModel(value = "公司端,门诊端-产品激活报表参数")
+@ApiModel(value = "公司端,,门诊端-产品激活报表参数")
 @Data
 public class CouponActiveQuery extends PageQuery {
     @ApiModelProperty(value = "产品名称")
