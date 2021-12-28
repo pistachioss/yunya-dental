5dec43f92fb3f7c810cd061a7baa06a86118115e
me <1123038904@qq.com>
2021-12-28 12:49:49 +0800

    激活门诊增加筛选

--- yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/mapper/BaseCouponMapper.java
+++ yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/mapper/BaseCouponMapper.java
@@ -32,7 +32,7 @@ public interface BaseCouponMapper extends Mapper<BaseCoupon> {
 	 */
 	List<RechargeVo> listRechargeByParam(@Param("couponName") String couponName, @Param("couponCategoryIds") List<Integer> couponCategoryIds);
 
-	List<CouponActiveVo> listCouponActive(@Param("couponName") String couponName, @Param("soldChannelIds") List<Integer> soldChannelIds,
+	List<CouponActiveVo> listCouponActive(@Param("couponIds") List<Integer> couponIds, @Param("soldChannelIds") List<Integer> soldChannelIds,
 										  @Param("activeOrgIds") List<Integer> activeOrgIds,
 										  @Param("activeStartDate") String activeStartDate,@Param("activeEndDate") String activeEndDate);
 
