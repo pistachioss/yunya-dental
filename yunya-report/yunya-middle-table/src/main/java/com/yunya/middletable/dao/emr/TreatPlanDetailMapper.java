8d998fff80f8c8ff42dd7fad92977ecba84e66f4
chenlin <chenlinpsv@163.com>
2022-01-15 15:18:36 +0800

    [新增功能](dev_2.2.2): 需求1469：门诊端电子病历迭代
    1、增加中间表同步：暂存开单、解锁后的修改订单、调整账单等操作时，同步更新治疗计划详情的状态

---
+++ yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/dao/emr/TreatPlanDetailMapper.java
@@ -0,0 +1,7 @@
+package com.yunya.middletable.dao.emr;
+
+import com.yunya.models.emr.TreatPlanDetail;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface TreatPlanDetailMapper extends Mapper<TreatPlanDetail> {
+}
