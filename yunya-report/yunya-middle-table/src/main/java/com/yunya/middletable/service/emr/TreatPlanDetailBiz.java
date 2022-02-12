624fd588ce53ed587502a140d0d8bef02af35bc9
chenlin <chenlinpsv@163.com>
2022-02-12 13:00:39 +0800

    [代码完善](dev_2.2.2): 修复bug
    开单时，记录治疗计划与订单项目核销关系报错

--- yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/service/emr/TreatPlanDetailBiz.java
+++ yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/service/emr/TreatPlanDetailBiz.java
@@ -7,12 +7,11 @@ import com.yunya.framework.common.biz.BaseBiz;
 import com.yunya.framework.common.utils.StringHelper;
 import com.yunya.middletable.dao.emr.TreatPlanDetailMapper;
 import com.yunya.middletable.dao.emr.TreatPlanDetailWriteoffMapper;
-import com.yunya.middletable.dao.emr.TreatPlanRecordMapper;
 import com.yunya.middletable.dao.treatment.BillPayRecordMapper;
 import com.yunya.middletable.dao.treatment.OrderDetailMapper;
 import com.yunya.middletable.dao.treatment.OrderRecordMapper;
+import com.yunya.models.emr.TreatPlanDetail;
 import com.yunya.models.emr.TreatPlanDetailWriteoff;
-import com.yunya.models.emr.TreatPlanRecord;
 import com.yunya.models.treatment.BillPayRecord;
 import com.yunya.models.treatment.OrderDetail;
 import com.yunya.models.treatment.OrderRecord;
@@ -35,7 +34,7 @@ import java.util.stream.Collectors;
  * @since: 1.0.0
  */
 @Service
-public class TreatPlanRecordBiz extends BaseBiz<TreatPlanRecordMapper, TreatPlanRecord> {
+public class TreatPlanDetailBiz extends BaseBiz<TreatPlanDetailMapper, TreatPlanDetail> {
 
     @Autowired
     private OrderRecordMapper orderRecordMapper;
