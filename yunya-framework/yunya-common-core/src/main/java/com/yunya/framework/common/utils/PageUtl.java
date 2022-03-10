0b777f5852f9230722fcb44c511afdcca9132bf8
chenlin <chenlinpsv@163.com>
2022-03-10 16:41:13 +0800

    [Bug修复](beta): 修复bug
    api/treatment/list接口报错500，分页数据不正确

--- yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/utils/PageUtl.java
+++ yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/utils/PageUtl.java
@@ -3,6 +3,7 @@ package com.yunya.framework.common.utils;
 import com.github.pagehelper.PageInfo;
 import com.yunya.framework.common.model.PageQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,8 +19,7 @@ public class PageUtl<T> {
     /**
      * 手动分页（默认分页）
      *
-     * @param pageNum 第几页
-     * @param pageSize 条数
+     * @param pageQuery
      * @param resultList 数据集
      * @return
      */
@@ -53,12 +53,16 @@ public class PageUtl<T> {
             return new PageInfo<>(resultList);
         }
         int total = resultList.size();
+        int fromInd = pageSize * (pageNum - 1);
+        int toInd = Math.min((pageSize * pageNum), total);
         PageInfo<T> pageInfo = new PageInfo<>();
         pageInfo.setPageNum(pageNum);
         pageInfo.setPageSize(pageSize);
         pageInfo.setTotal(total);
-        List<T> list =
-                resultList.subList(pageSize * (pageNum - 1), (Math.min((pageSize * pageNum), total)));
+        List<T> list = new ArrayList<>();
+        if (fromInd < toInd) {
+            list = resultList.subList(fromInd, toInd);
+        }
         pageInfo.setList(list);
         return pageInfo;
     }
