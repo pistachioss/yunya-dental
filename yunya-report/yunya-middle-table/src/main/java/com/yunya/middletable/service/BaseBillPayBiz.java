cf05d7aaad03870da703909844e47ad1756d24e0
lihuibin <951755155@qq.com>
2022-03-18 15:00:29 +0800

    [Bug修复](dev): 修复积分新增失败问题

--- yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/service/BaseBillPayBiz.java
+++ yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/service/BaseBillPayBiz.java
@@ -31,10 +31,13 @@ import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.function.Function;
 
 /**
  * 简介: 中间表收费记录处理业务层
@@ -69,6 +72,11 @@ public class BaseBillPayBiz extends BaseBiz<BaseBillPayMapper, BaseBillPay> {
   private ExecutorService importExcelThreadPool;
   @Resource(name = "billCreditsCallbackImpl")
   private BillCreditsCallback billCreditsCallback;
+  private Map<Integer,BillCreditsCallback> chain = new HashMap<>();
+
+  public void addCallBack(Integer bId,BillCreditsCallback callback) {
+    this.chain.put(bId,callback);
+  }
 
   /** 本次免单支付 */
   private final Integer PAYMENT_BY_CUSTOMER_FREE = 23;
@@ -92,7 +100,16 @@ public class BaseBillPayBiz extends BaseBiz<BaseBillPayMapper, BaseBillPay> {
         if (null != baseBillPay) {
           mapper.insertSelective(baseBillPay);
           // 保存收费记录明细
+          log.info("保存收费记录明细baseBillPay: {}",baseBillPay);
           saveBillPayDetailRecord(dataId);
+          if(!chain.isEmpty()){
+            for (Map.Entry<Integer, BillCreditsCallback> entry : chain.entrySet()) {
+              // 回调积分增加方法，
+              // 这里存在消息消费顺序性问题，为了防止正常业务读不到账单支付记录的情况而导致新增积分失败，所以这里使用回调的方式将积分做新增操作
+              // 回调的逻辑查看BaseBillBiz.java 中baseBillPayBiz.addCallBack(...)方法
+              entry.getValue().baseBillBizHandlerFinish(entry.getKey());
+            }
+          }
         } else {
           // 撤销积分
           billCreditsCallback.scrapCredits(dataId);
