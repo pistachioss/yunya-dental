2c8120b955449271d4b16d06124cdddd2b93bc17
chenlin <chenlinpsv@163.com>
2022-04-02 17:04:25 +0800

    [新增功能](dev_2.2.6): 需求1544：APP，推送消息，增加消息管理（历史消息查看，消息已读未读）
    第三方（极光）推送成功时，增加推送消息的记录

--- yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/util/JpushManager.java
+++ yunya-modules/yunya-employee-attend/src/main/java/com/yunya/modules/employeeattend/util/JpushManager.java
@@ -204,11 +204,23 @@ public class JpushManager {
 
   private String getTypeName(int type) {
     switch (type) {
-      case 1:
+      case 10:
+      case 11:
+      case 12:
+      case 13:
+      case 14:
         return "请假";
-      case 2:
+      case 20:
+      case 21:
+      case 22:
+      case 23:
+      case 24:
         return "加班";
-      case 3:
+      case 30:
+      case 31:
+      case 32:
+      case 33:
+      case 34:
         return "外勤";
     }
     return "";
