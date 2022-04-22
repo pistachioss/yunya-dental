4a4e24f4ec5ca75ac9307582be273d7d6b01952c
chenlin <chenlinpsv@163.com>
2022-04-22 10:43:31 +0800

    [新增功能](dev_2.2.7): 需求
    查询门诊挂号患者的手机号，生成txt文件

--- yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/dao/report/BasePatientGroupRelationMapper.java
+++ yunya-report/yunya-middle-table/src/main/java/com/yunya/middletable/dao/report/BasePatientGroupRelationMapper.java
@@ -1,7 +1,17 @@
 package com.yunya.middletable.dao.report;
 
+import com.yunya.models.report.BasePatient;
 import com.yunya.models.report.BasePatientGroupRelation;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 public interface BasePatientGroupRelationMapper extends Mapper<BasePatientGroupRelation> {
+
+    List<BasePatient> selectRegisterdPatientMobile(
+            @Param("sDate") String sDate,
+            @Param("eDate") String eDate,
+            @Param("orgId") Integer orgId,
+            @Param("mobiles") List<String> mobiles);
 }
