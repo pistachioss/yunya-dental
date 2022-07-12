51c077fd8f49f983040757682918a5867d317e81
chenlin <chenlinpsv@163.com>
2022-07-12 15:08:52 +0800

    [Bug修复](dev_2.3.1): 修复bug: 365产品售卖激活统计表导出文件没有文件名
    修复bug: 365产品售卖激活统计表导出文件没有文件名

--- yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/biz/BaseCardBiz.java
+++ yunya-report/yunya-report-ultimate/src/main/java/com/yunya/report/ultimate/biz/BaseCardBiz.java
@@ -162,7 +162,8 @@ public class BaseCardBiz extends BaseBiz<BaseCardMapper, BaseCard> {
     List<Coupon365SoldActivedStatisticsVO> list = coupon365SoldActivedStatistics(query).getList();
     ExcelUtil<Coupon365SoldActivedStatisticsVO> excelUtil =
             new ExcelUtil<>(Coupon365SoldActivedStatisticsVO.class);
-    excelUtil.exportExcel(response, list, "365卡产品售出激活统计表");
+    String fileName = "365卡产品售出激活统计表";
+    excelUtil.exportExcel(response, list, fileName, fileName);
   }
 
   /**
