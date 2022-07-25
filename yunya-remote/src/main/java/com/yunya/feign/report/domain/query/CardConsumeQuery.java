be2e5e9067f84e964948cf79d14fbe797ff84457
zhouwen <sa52305@outlook.com>
2022-07-25 10:38:35 +0800

    [需求实现](dev): 更新查询卡券使用记录列表

--- yunya-remote/src/main/java/com/yunya/feign/report/domain/query/CardConsumeQuery.java
+++ yunya-remote/src/main/java/com/yunya/feign/report/domain/query/CardConsumeQuery.java
@@ -5,9 +5,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -34,10 +32,8 @@ public class CardConsumeQuery extends PageQuery {
 
   @ApiModelProperty(value = "销售渠道ID", required = true)
   @NotNull(message = "请选择销售渠道")
-  @Length(min = 1,message = "非法请求参数")
   private Integer[] saleChannelIds;
 
   @ApiModelProperty("产品ID")
-  @Min(value = 1, message = "非法请求参数")
   private Integer[] couponId;
 }
