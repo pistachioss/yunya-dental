7f616998900814f28a2106e739d081a95a3d776d
xiangyang <xiangyang@qq.com>
2022-08-12 16:53:23 +0800

    feat(ivy-mini): 订单 订单

--- yunya-remote/src/main/java/com/yunya/feign/ivy_mini/domain/vo/VirtualActiveVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/ivy_mini/domain/vo/VirtualActiveVO.java
@@ -1,5 +1,6 @@
 package com.yunya.feign.ivy_mini.domain.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -14,5 +15,6 @@ public class VirtualActiveVO {
     @ApiModelProperty(value = "手机号")
     private String mobile;
     @ApiModelProperty(value = "激活时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private LocalDateTime activeDate;
 }
