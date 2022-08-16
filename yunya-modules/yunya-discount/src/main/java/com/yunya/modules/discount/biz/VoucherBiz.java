dbaa24d2610c0dfddd32695d614580feda35ff61
me <1123038904@qq.com>
2022-08-16 14:55:37 +0800

    123

--- yunya-modules/yunya-discount/src/main/java/com/yunya/modules/discount/biz/VoucherBiz.java
+++ yunya-modules/yunya-discount/src/main/java/com/yunya/modules/discount/biz/VoucherBiz.java
@@ -128,6 +128,7 @@ public class VoucherBiz extends BaseBiz<VoucheCouponMapper, VoucheCoupon> {
 
             couponCommonInfo.setAvailableSaleStartDate(discountUpdateForm.getAvailableSaleStartDate());
             couponCommonInfo.setAvailableSaleEndDate(discountUpdateForm.getAvailableSaleEndDate());
+            couponCommonInfo.setIsOnlineSale(discountUpdateForm.getIsOnlineSale());
             //更新基础信息
             couponCommonInfoBiz.updateById(couponCommonInfo);
             voucheCoupon.setCouponId(discountUpdateForm.getId());
