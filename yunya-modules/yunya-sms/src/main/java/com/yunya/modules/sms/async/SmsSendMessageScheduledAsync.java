d0d2ac6a72ba31807d5f2cf44b78464ff2c8f7b1
chenlin <chenlinpsv@163.com>
2022-08-18 09:43:08 +0800

    修复bug: sms短信队列堵塞

--- yunya-modules/yunya-sms/src/main/java/com/yunya/modules/sms/async/SmsSendMessageScheduledAsync.java
+++ yunya-modules/yunya-sms/src/main/java/com/yunya/modules/sms/async/SmsSendMessageScheduledAsync.java
@@ -11,6 +11,7 @@ import com.yunya.framework.common.constant.RedisConstants;
 import com.yunya.framework.common.utils.StringHelper;
 import com.yunya.framework.redis.util.RedisUtils;
 import com.yunya.modules.sms.biz.SmsSendRecordBiz;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,10 +34,10 @@ import static com.yunya.framework.common.constant.BusinessConstants.COMPANY_ORGI
  * @Date: 2020/12/15 15:17
  * @since: 1.0.0
  */
+@Slf4j
 @Component
 @EnableScheduling
 public class SmsSendMessageScheduledAsync {
-    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private SmsSendRecordBiz smsSendRecordBiz;
@@ -51,11 +52,12 @@ public class SmsSendMessageScheduledAsync {
      * 每隔5秒执行一次：
      * 消费redis队列的短信数据
      */
-    @Async("customizeExecutor")
-    @Scheduled(cron = "*/5 * * * * ?")
+//    @Async("customizeExecutor")
+    @Scheduled(cron = "0/5 * * * * ?")
     public void smsSendMessageAsyncByEventCode() {
+        smsSendVerifyCodeAsync();
         List<OrganizationInfoDetail> orgInfos = getOrganizationList();
-        log.info("开始处理短信队列数据：门诊：{}", JSONObject.toJSON(orgInfos));
+        log.info("开始处理各门诊短信队列数据：门诊：{}", JSONObject.toJSON(orgInfos));
         if (StringHelper.isEmpty(orgInfos)) {
             return;
         }
@@ -73,7 +75,6 @@ public class SmsSendMessageScheduledAsync {
 //                log.error("smsSendMessageAsync error", e);
 //            }
 //        }));
-
         orgInfos.forEach(orgInfo->{
             SmsAutoEventSendRecordModel smsAutoEventSendRecordModel = redisUtils.rPop(
                     RedisConstants.SMS_SEND_MESSAGE_QUEUE + orgInfo.getId(), SmsAutoEventSendRecordModel.class);
@@ -96,8 +97,8 @@ public class SmsSendMessageScheduledAsync {
      * 每隔5秒执行一次：
      * 消费redis队列的短信数据
      */
-    @Async("customizeExecutor")
-    @Scheduled(cron = "*/5 * * * * ?")
+//    @Async("customizeExecutor")
+//    @Scheduled(cron = "*/5 * * * * ?")
     public void smsSendMessageAsyncByTemplateId() {
         SmsTemplateIdRecordModel smsTemplateIdRecordModel = redisUtils.rPop(
                 RedisConstants.SMS_SEND_MESSAGE_QUEUE, SmsTemplateIdRecordModel.class);
@@ -116,18 +117,15 @@ public class SmsSendMessageScheduledAsync {
     }
 
     /**
-     * 每隔5秒执行一次：
      * 消费redis队列的短信验证码
      */
-    @Async("customizeExecutor")
-    @Scheduled(cron = "*/5 * * * * ?")
     public void smsSendVerifyCodeAsync() {
         SmsVerifyCodeModel smsVerifyCodeModel = redisUtils.rPop(
                 RedisConstants.SMS_SEND_VERIFYCODE_QUEUE + COMPANY_ORGID, SmsVerifyCodeModel.class);
+        log.info("开始处理短信验证码：{}", JSONObject.toJSONString(smsVerifyCodeModel));
         if (smsVerifyCodeModel == null) {
             return;
         }
-        log.info("开始处理短信验证码：{}", JSONObject.toJSONString(smsVerifyCodeModel));
         try {
             smsSendRecordBiz.sendVerifyCode(smsVerifyCodeModel);
             log.info("处理短信验证码完成");
