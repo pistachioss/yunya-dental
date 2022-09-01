99ef6b7858548ebda28c1dfdc0fdeabf3dae1138
me <1123038904@qq.com>
2022-09-01 14:41:12 +0800

    123

--- yunya-remote/src/main/java/com/yunya/feign/patient_central/domain/vo/web/WxWechatbindListVO.java
+++ yunya-remote/src/main/java/com/yunya/feign/patient_central/domain/vo/web/WxWechatbindListVO.java
@@ -18,6 +18,8 @@ public class WxWechatbindListVO {
     private String name;
     @ApiModelProperty("患者关系名称 用来判断是否为本人 区分微信拥有者")
     private String dictionaryName;
+    @ApiModelProperty("手机")
+    private String mobile;
     @ApiModelProperty("字典Id")
     private Integer dictionaryId;
 }
