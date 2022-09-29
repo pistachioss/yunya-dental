006114bb36112b42729cc53a6eacb24e1ae02fde
ChordKey <chtitaxie@hotmail.com>
2022-09-29 22:05:49 +0800

    fix: add new patient default inservice.

--- yunya-modules/yunya-patient-central/src/main/java/com/yunya/modules/patient_central/biz/PatientBaseInfoBiz.java
+++ yunya-modules/yunya-patient-central/src/main/java/com/yunya/modules/patient_central/biz/PatientBaseInfoBiz.java
@@ -199,6 +199,7 @@ public class PatientBaseInfoBiz extends BaseBiz<PatientBaseInfoMapper, PatientBa
       }
     }
     patientBaseInfo.setPinyinName(HanyuPinyinHelper.toHanyuPinyin(patientBaseInfo.getName()));
+    patientBaseInfo.setInservice(true); //默认有效
     patientBaseInfo.setCrtId(Integer.parseInt(BaseContextHandler.getUserID()));
     patientBaseInfo.setCrtName(BaseContextHandler.getName());
     patientBaseInfo.setOrgId(Integer.parseInt(BaseContextHandler.getOrgId()));
