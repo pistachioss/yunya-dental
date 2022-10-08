854fbacb59d569aa99b0a2f4ac2d1d12726465e5
xiangyang <xiangyang@qq.com>
2022-10-08 17:42:50 +0800

    feat(qzt): 全诊通 门诊 病例 项目

--- yunya-modules/yunya-emr/src/main/java/com/yunya/modules/emr/task/Task.java
+++ yunya-modules/yunya-emr/src/main/java/com/yunya/modules/emr/task/Task.java
@@ -1,5 +1,6 @@
 package com.yunya.modules.emr.task;
 
+import com.yunya.modules.emr.biz.QztItemBiz;
 import com.yunya.modules.emr.biz.QztMedicalBiz;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -22,6 +23,8 @@ public class Task {
 
     @Resource
     private QztMedicalBiz qztMedicalBiz;
+    @Resource
+    private QztItemBiz qztItemBiz;
 
     @Scheduled(cron = "00 00 01 * * ?")
     @RequestMapping("/white/qztMedical/sync")
