45226935c9e68459f9f0d1350e2519de5e889ef3
me <1123038904@qq.com>
2022-10-14 15:47:26 +0800

    123

--- yunya-tools/ivy-mini/src/main/java/com/yunya365/mini/service/impl/OrderAdminiServiceImpl.java
+++ yunya-tools/ivy-mini/src/main/java/com/yunya365/mini/service/impl/OrderAdminiServiceImpl.java
@@ -55,7 +55,7 @@ public class OrderAdminiServiceImpl extends BaseBiz<OrderInfoMapper, OrderInfo> 
         List<OrderVO> result = mapper.findOrderList(form);
         for (OrderVO a : result) {
             a.setReceivingInformation(a.getReceiverName() + " " + a.getReceiverPhone() + " " + a.getAddress());
-            if(a.getStatus()==3&&a.getActiveStatus()==0&&a.getProductType()==0){
+            if(a.getStatus()==3&&a.getActiveStatus()==0&&a.getProductType()==1){
                 a.setStatus(new Byte("7"));
             }
         }
