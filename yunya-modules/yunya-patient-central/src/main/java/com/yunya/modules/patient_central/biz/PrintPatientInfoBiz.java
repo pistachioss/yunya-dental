fe1c498bf17d91a76358d509e0ea7cce51969a67
me <1123038904@qq.com>
2022-10-25 15:58:06 +0800

    123

--- yunya-modules/yunya-patient-central/src/main/java/com/yunya/modules/patient_central/biz/PrintPatientInfoBiz.java
+++ yunya-modules/yunya-patient-central/src/main/java/com/yunya/modules/patient_central/biz/PrintPatientInfoBiz.java
@@ -9,16 +9,20 @@ import com.yunya.feign.system.RemoteSystemServiceFeign;
 import com.yunya.feign.treatment.RemoteTreatmentServiceFeign;
 import com.yunya.feign.treatment.domain.vo.LastTreatmentInfoVO;
 import com.yunya.feign.treatment.domain.vo.TreatmentRecordExtendVO;
+import com.yunya.framework.common.utils.DateUtil;
 import com.yunya.framework.common.utils.StringHelper;
 import com.yunya.models.patient_central.PatientBaseInfo;
 import com.yunya.models.system.MemberType;
 import com.yunya.models.system.SysEmployee;
 import com.yunya.models.treatment.TreatmentRecord;
 import com.yunya.modules.patient_central.mapper.PatientExpInfoMapper;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -52,10 +56,23 @@ public class PrintPatientInfoBiz {
         // 设置患者名字和病历编号
         PatientTotalInfoVo patientTotalInfo = this.patientBaseInfoBiz.findPatientTotalInfo(patientId);
         PatientExpInfoVo expInfo = patientExpInfoMapper.selectByPatientId(patientId);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date birthday = null;
+        try {
+            birthday = simpleDateFormat.parse(patientTotalInfo.getBirthday());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        if (birthday != null) {
+            // 计算年龄
+            Integer age = DateUtil.differFromDate(birthday, new Date(System.currentTimeMillis()));
+            printInfoVo.setAge(age);
+        }
+
         printInfoVo.setMedicalNumber(patientTotalInfo.getMedicalNumber());
         printInfoVo.setPatientName(patientTotalInfo.getName());
         printInfoVo.setAddress(expInfo.getAddress());
-        printInfoVo.setAge(patientTotalInfo.getAge());
+//        printInfoVo.setAge(patientTotalInfo.getAge());
         printInfoVo.setCity(expInfo.getCity());
         printInfoVo.setCountry(expInfo.getCountry());
         printInfoVo.setProvince(expInfo.getProvince());
