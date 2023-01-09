6e8cb842bfbb0e2c24e4dbfc2c3624ba0b9604be
ChordKey <chtitaxie@hotmail.com>
2023-01-09 16:28:03 +0800

    add: #1855 for birthday check6.

--- yunya-remote/src/main/java/com/yunya/feign/report/domain/query/PatientManageQuery.java
+++ yunya-remote/src/main/java/com/yunya/feign/report/domain/query/PatientManageQuery.java
@@ -60,8 +60,8 @@ public class PatientManageQuery extends PageQuery {
     /** 患者分组Id*/
     @ApiModelProperty("患者分组id")
     private Integer patientGroupId;
-    @ApiModelProperty(value = "末诊门诊")
-    private Integer lastOrgId;
+    @ApiModelProperty(value = "末诊门诊列表")
+    private Integer[] lastOrgId;
     @ApiModelProperty(value = "生日时间")
     private String birthday;
 }
