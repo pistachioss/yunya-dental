f290e07c8a72e0ef9b3fe9a427e22363d600fa27
chenlin <chenlinpsv@163.com>
2023-02-20 09:43:24 +0800

    修复bug: 线上患者“王梦瑶”修改亲属关系“陈渃彤”报错

--- yunya-modules/yunya-patient-central/src/main/java/com/yunya/modules/patient_central/biz/PatientKinRelationBiz.java
+++ yunya-modules/yunya-patient-central/src/main/java/com/yunya/modules/patient_central/biz/PatientKinRelationBiz.java
@@ -162,6 +162,7 @@ public class PatientKinRelationBiz extends BaseBiz<PatientKinRelationMapper, Pat
       PatientKinRelation linkedPatient = new PatientKinRelation();
       linkedPatient.setPatientId(patientKinRelation.getLinkedPatientId());
       linkedPatient.setLinkedPatientId(patientKinRelation.getPatientId());
+      linkedPatient.setInservice(true);
       PatientKinRelation kinRelation = mapper.selectOne(linkedPatient);
       if (kinRelation != null){
         kinRelation.setKinshipId(patientKinRelation.getKinshipId());
