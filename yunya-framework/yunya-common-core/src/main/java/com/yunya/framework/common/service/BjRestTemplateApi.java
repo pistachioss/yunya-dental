e7ad75f67b2fe2db7752bf99daced6c1626cb19e
xiangyang <1480447105@qq.com>
2023-03-06 09:46:35 +0800

    feat(company): 滨江 接口对接

--- yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/service/BjRestTemplateApi.java
+++ yunya-framework/yunya-common-core/src/main/java/com/yunya/framework/common/service/BjRestTemplateApi.java
@@ -37,6 +37,7 @@ public class BjRestTemplateApi {
     }
 
     private JSONObject getRequestRes(String resultStr) {
+        log.error("滨江api返回结果：{}", resultStr);
         JSONObject jsonObject = JSONObject.parseObject(resultStr);
         boolean success = jsonObject.getBoolean("success");
         String message = jsonObject.getString("msg");
